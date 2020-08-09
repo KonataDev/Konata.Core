eb30b0f847a291e906f2878a52cf0c7d17d23d3e
TheSnowfield <thesnowfield@moezsoft.com>
2020-08-09 14:37:56 +0800

    commit files

--- konata.core/protocol/packet/oicq/OicqRequestTgtgt.cs
+++ konata.core/protocol/packet/oicq/OicqRequestTgtgt.cs
@@ -52,23 +52,23 @@ namespace Konata.Protocol.Packet.Oicq
 
             byte[] passwordMd5 = new Md5Cryptor().Encrypt(Encoding.UTF8.GetBytes(password));
 
-            tlvs.PushTlv(Tlv.T18(AppInfo.appId, AppInfo.appClientVersion, uin));
+            tlvs.PushTlv(new T18(AppInfo.appId, AppInfo.appClientVersion, uin));
             tlvs.PushTlv(new T1(uin, DeviceInfo.Network.Wifi.IpAddress));
-            tlvs.PushTlv(Tlv.T106(AppInfo.appId, AppInfo.subAppId, AppInfo.appClientVersion, uin,
+            tlvs.PushTlv(new T106(AppInfo.appId, AppInfo.subAppId, AppInfo.appClientVersion, uin,
                 new byte[4], true, passwordMd5, 0, tgtgKey, true, DeviceInfo.Guid, LoginType.Password));
-            tlvs.PushTlv(Tlv.T116(184024956, 66560));
-            tlvs.PushTlv(Tlv.T100(AppInfo.appId, AppInfo.subAppId, AppInfo.appClientVersion));
-            tlvs.PushTlv(Tlv.T142(AppInfo.apkPackageName));
+            tlvs.PushTlv(new T116(184024956, 66560));
+            tlvs.PushTlv(new T100(AppInfo.appId, AppInfo.subAppId, AppInfo.appClientVersion));
+            tlvs.PushTlv(new T142(AppInfo.apkPackageName));
             tlvs.PushTlv(Tlv.T144(DeviceInfo.System.AndroidId, reportData.ToArray(), DeviceInfo.System.Os,
                 DeviceInfo.System.OsVersion, DeviceInfo.Network.Type, DeviceInfo.Network.Mobile.OperatorName,
                 new byte[0], DeviceInfo.Network.Wifi.ApnName, true, true, false,
                 DeviceInfo.Guid, 0, DeviceInfo.System.ModelName, DeviceInfo.System.Manufacturer, tgtgKey));
-            tlvs.PushTlv(Tlv.T145(DeviceInfo.Guid));
-            tlvs.PushTlv(Tlv.T147(AppInfo.appId, AppInfo.apkVersionName, AppInfo.apkSignature));
+            tlvs.PushTlv(new T145(DeviceInfo.Guid));
+            tlvs.PushTlv(new T147(AppInfo.appId, AppInfo.apkVersionName, AppInfo.apkSignature));
             // tlvs.PushTlv(Tlv.166());
-            tlvs.PushTlv(Tlv.T154(0));
-            tlvs.PushTlv(Tlv.T141(DeviceInfo.Network.Mobile.OperatorName, DeviceInfo.Network.Type, DeviceInfo.Network.Wifi.ApnName));
-            tlvs.PushTlv(Tlv.T8());
+            tlvs.PushTlv(new T154(0));
+            tlvs.PushTlv(new T141(DeviceInfo.Network.Mobile.OperatorName, DeviceInfo.Network.Type, DeviceInfo.Network.Wifi.ApnName));
+            tlvs.PushTlv(new T8());
             tlvs.PushTlv(Tlv.T511(new string[]
             {
                 "game.qq.com",
@@ -87,13 +87,13 @@ namespace Konata.Protocol.Packet.Oicq
                 "vip.qq.com",
                 "gamecenter.qq.com",
             }));
-            tlvs.PushTlv(Tlv.T187(DeviceInfo.Network.Wifi.MacAddress));
-            tlvs.PushTlv(Tlv.T188(Hex.HexStr2Bytes(DeviceInfo.System.AndroidId)));
+            tlvs.PushTlv(new T187(DeviceInfo.Network.Wifi.MacAddress));
+            tlvs.PushTlv(new T188(Hex.HexStr2Bytes(DeviceInfo.System.AndroidId)));
             // tlvs.PushTlv(Tlv.194());
-            tlvs.PushTlv(Tlv.T191());
+            tlvs.PushTlv(new T191());
             tlvs.PushTlv(Tlv.T202(DeviceInfo.Network.Wifi.ApMacAddress, DeviceInfo.Network.Wifi.Ssid));
             tlvs.PushTlv(Tlv.T177());
-            tlvs.PushTlv(Tlv.T516());
+            tlvs.PushTlv(new T516());
             tlvs.PushTlv(Tlv.T521());
             tlvs.PushTlv(Tlv.T525(Tlv.T536(new byte[] { 0x01, 0x00 })));
 
