9d860f7c8e12070576345c665f42e85868795eb6
jasonbest <jasonbest@vip.qq.com>
2020-08-09 08:03:49 +0000

    split tlv 177 202 511 521

---
+++ konata.core/protocol/packet/tlvs/T177.cs
@@ -0,0 +1,30 @@
+﻿using Konata.Utils;
+
+namespace Konata.Protocol.Packet.Tlvs
+{
+    public class T177 : TlvBase
+    {
+        private readonly long _buildTime;
+        private readonly string _sdkVersion;
+
+        public T177(long buildTime = 1577331209, string sdkVersion = "6.0.0.2425")
+        {
+            _buildTime = buildTime;
+            _sdkVersion = sdkVersion;
+        }
+
+        public override ushort GetTlvCmd()
+        {
+            return 0x177;
+        }
+
+        public override byte[] GetTlvBody()
+        {
+            StreamBuilder builder = new StreamBuilder();
+            builder.PushInt8(1);
+            builder.PushInt32((int)_buildTime);
+            builder.PushString(_sdkVersion);
+            return builder.GetPlainBytes();
+        }
+    }
+}
---
+++ konata.core/protocol/packet/tlvs/T521.cs
@@ -0,0 +1,29 @@
+﻿using Konata.Utils;
+
+namespace Konata.Protocol.Packet.Tlvs
+{
+    public class T521:TlvBase
+    {
+        private readonly int _productType;
+        private readonly short _unknown;
+
+        public T521(int productType = 0, short unknown = 0)
+        {
+            _productType = productType;
+            _unknown = unknown;
+        }
+
+        public override ushort GetTlvCmd()
+        {
+            return 0x521;
+        }
+
+        public override byte[] GetTlvBody()
+        {
+            StreamBuilder builder = new StreamBuilder();
+            builder.PushInt32(_productType);
+            builder.PushInt16(_unknown);
+            return builder.GetPlainBytes();
+        }
+    }
+}
