cc587b4ed4e123b50b6109f96ea5d9ea35b4eef4
TheSnowfield <thesnowfield@moezsoft.com>
2020-08-10 05:29:34 +0800

    commit files

--- konata.core/protocol/packet/ToServicePacket.cs
+++ konata.core/protocol/packet/ToServicePacket.cs
@@ -1,12 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Konata.Protocol.Packet
 {
-    class OutGoingPacket : PacketBase
+    public class ToServicePacket : PacketBase
     {
         private byte[] data;
 
--- konata.core/protocol/packet/oicq/OicqRequestTgtgt.cs
+++ konata.core/protocol/packet/oicq/OicqRequestTgtgt.cs
@@ -15,6 +15,8 @@ namespace Konata.Protocol.Packet.Oicq
         private ulong _uin;
         private string _password;
         private byte[] _tgtgKey;
+        private byte[] _randKey = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
+        private byte[] _shareKey = { 0x4D, 0xA0, 0xF6, 0x14, 0xFC, 0x9F, 0x29, 0xC2, 0x05, 0x4C, 0x77, 0x04, 0x8A, 0x65, 0x66, 0xD7 };
 
         public OicqRequestTgtgt(ulong botUin, string botPassword, byte[] tgtgKey)
         {
@@ -29,24 +31,26 @@ namespace Konata.Protocol.Packet.Oicq
 
         public override byte[] GetBytes()
         {
-            TlvPacker tlvs = new TlvPacker();
 
-            DeviceReport deviceReport = new DeviceReport();
-            deviceReport.Bootloader = Encoding.UTF8.GetBytes(DeviceInfo.Build.Bootloader);
-            deviceReport.Version = Encoding.UTF8.GetBytes(DeviceInfo.System.OsVersion);
-            deviceReport.CodeName = Encoding.UTF8.GetBytes(DeviceInfo.Build.CodeName);
-            deviceReport.Incremental = Encoding.UTF8.GetBytes(DeviceInfo.Build.Incremental);
-            deviceReport.Fingerprint = Encoding.UTF8.GetBytes(DeviceInfo.Build.Fingerprint);
-            deviceReport.BootId = Encoding.UTF8.GetBytes(DeviceInfo.BootId);
-            deviceReport.AndroidId = Encoding.UTF8.GetBytes(DeviceInfo.System.AndroidId);
-            deviceReport.BaseBand = Encoding.UTF8.GetBytes(DeviceInfo.Build.BaseBand);
-            deviceReport.InnerVersion = Encoding.UTF8.GetBytes(DeviceInfo.Build.InnerVersion);
-
+            DeviceReport deviceReport = new DeviceReport
+            {
+                Bootloader = Encoding.UTF8.GetBytes(DeviceInfo.Build.Bootloader),
+                Version = Encoding.UTF8.GetBytes(DeviceInfo.System.OsVersion),
+                CodeName = Encoding.UTF8.GetBytes(DeviceInfo.Build.CodeName),
+                Incremental = Encoding.UTF8.GetBytes(DeviceInfo.Build.Incremental),
+                Fingerprint = Encoding.UTF8.GetBytes(DeviceInfo.Build.Fingerprint),
+                BootId = Encoding.UTF8.GetBytes(DeviceInfo.BootId),
+                AndroidId = Encoding.UTF8.GetBytes(DeviceInfo.System.AndroidId),
+                BaseBand = Encoding.UTF8.GetBytes(DeviceInfo.Build.BaseBand),
+                InnerVersion = Encoding.UTF8.GetBytes(DeviceInfo.Build.InnerVersion)
+            };
             MemoryStream reportData = new MemoryStream();
             Serializer.Serialize(reportData, deviceReport);
 
             byte[] passwordMd5 = new Md5Cryptor().Encrypt(Encoding.UTF8.GetBytes(_password));
 
+            // 構建 tlv
+            TlvPacker tlvs = new TlvPacker();
             tlvs.PushTlv(new T18(AppInfo.appId, AppInfo.appClientVersion, _uin));
             tlvs.PushTlv(new T1(_uin, DeviceInfo.Network.Wifi.IpAddress));
             tlvs.PushTlv(new T106(AppInfo.appId, AppInfo.subAppId, AppInfo.appClientVersion, _uin,
@@ -66,21 +70,20 @@ namespace Konata.Protocol.Packet.Oicq
             tlvs.PushTlv(new T8());
             tlvs.PushTlv(new T511(new string[]
             {
-                "game.qq.com",
-                "mail.qq.com",
-                "qzone.qq.com",
-                "qun.qq.com",
-                "openmobile.qq.com",
-                "tenpay.com",
-                "connect.qq.com",
-                "qun.qq.com",
-                "qqweb.qq.com",
                 "office.qq.com",
-                "ti.qq.com",
-                "mma.qq.com",
-                "docs.qq.com",
-                "vip.qq.com",
+                "qun.qq.com",
                 "gamecenter.qq.com",
+                "docs.qq.com",
+                "mail.qq.com",
+                "ti.qq.com",
+                "vip.qq.com",
+                "tenpay.com",
+                "qqweb.qq.com",
+                "qzone.qq.com",
+                "mma.qq.com",
+                "game.qq.com",
+                "openmobile.qq.com",
+                "connect.qq.com",
             }));
             tlvs.PushTlv(new T187(DeviceInfo.Network.Wifi.MacAddress));
             tlvs.PushTlv(new T188(DeviceInfo.System.AndroidId));
@@ -92,10 +95,36 @@ namespace Konata.Protocol.Packet.Oicq
             tlvs.PushTlv(new T521());
             tlvs.PushTlv(new T525(new T536(new byte[] { 0x01, 0x00 })));
 
-            // 構建oicq_request
-            StreamBuilder builder = new StreamBuilder();
+            // 構建 oicq_request 包躰
+            StreamBuilder requestBody = new StreamBuilder();
+            requestBody.PushInt16(_subCmd);
+            requestBody.PushBytes(tlvs.GetPacket(true), false);
 
-            return tlvs.GetPacket(true);
+            // 構建 oicq_request
+            StreamBuilder request = new StreamBuilder();
+            request.PushInt8(0x02); // 頭部 0x02
+            request.PushInt16(27 + 2);
+            request.PushInt16(8001); // 協議版本 1F 41
+            request.PushInt16(_cmd);
+            request.PushInt16(1);
+            request.PushInt32((int)_uin);
+            request.PushInt8(0x03);
+            request.PushUInt8(0x87); // 加密方式id
+            request.PushInt8(0x00); // 永遠0
+            request.PushInt32(2);
+            request.PushInt32(AppInfo.appClientVersion);
+            request.PushInt32(0);
+
+            // 密鑰
+            request.PushInt16(0x0101);
+            request.PushBytes(_randKey, false);
+            request.PushInt16(0x0102);
+            request.PushBytes(new byte[0], false, true);
+
+            request.PushBytes(requestBody.GetEncryptedBytes(new TeaCryptor(), _shareKey), false);
+            request.PushInt8(0x03); // 尾部 0x03
+
+            return request.GetPlainBytes();
         }
     }
 }
