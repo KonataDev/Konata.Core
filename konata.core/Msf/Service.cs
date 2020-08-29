b466f8aaec6b35bfb009d8e47aafe4ec000f434e
TheSnowfield <thesnowfield@moezsoft.com>
2020-08-30 04:10:18 +0800

    commit files

---
+++ konata.core/Msf/Service.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Konata.Msf
+{
+    internal abstract class Service
+    {
+        internal static bool Run(Core core) => false;
+
+        internal static bool Handle(byte[] data, Core core) => false;
+    }
+}
