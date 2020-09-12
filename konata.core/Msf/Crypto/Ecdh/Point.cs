2480069e99def4a4c4e7846899d462e62a0e44ed
TheSnowfield <thesnowfield@moezsoft.com>
2020-09-12 20:00:22 +0800

    ECDH implementation

---
+++ konata.core/Msf/Crypto/Ecdh/Point.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+
+namespace ECDHAlgorithm
+{
+    using bint = BigInteger;
+
+    internal struct Point
+    {
+        public bint X { get; set; }
+
+        public bint Y { get; set; }
+
+        public Point(bint x, bint y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public bool IsDefault => X == 0 && Y == 0;
+
+        public override string ToString() => $"({X:X}, {Y:X})";
+
+        public static Point operator -(Point p) => new Point(-p.X, -p.Y);
+    }
+}
