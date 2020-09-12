2af60827437b142158390064ad9f0884611e4998
TheSnowfield <thesnowfield@moezsoft.com>
2020-09-12 17:41:17 +0800

    add project files

---
+++ konata.debug/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+using Konata;
+using Konata.Utils;
+
+namespace Konata.Debug
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            byte[] output = { };
+
+            Bot bot = new Bot(2051118019, "12345678");
+            var _ = bot.Login();
+
+            Console.Read();
+        }
+    }
+
+}
