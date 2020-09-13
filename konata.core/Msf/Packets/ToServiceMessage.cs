a7f74d684408b61f5f2d77647fb542169c794920
TheSnowfield <thesnowfield@moezsoft.com>
2020-09-14 01:55:50 +0800

    fix to service message

---
+++ konata.core/Msf/Packets/ToServiceMessage.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Konata.Msf.Utils.Crypt;
+
+namespace Konata.Msf.Packets
+{
+    public class ToServiceMessage : Packet
+    {
+        private Body _body;
+        private Header _header;
+
+        public ToServiceMessage(uint packetType, uint encryptType, uint uin, Packet packet)
+        {
+            _body = new Body(packet, new TeaCryptor(), new byte[16]);
+            _header = new Header(packetType, encryptType, new byte[0], uin.ToString());
+        }
+
+        public override byte[] GetBytes()
+        {
+            PutUintBE((uint)(_header.Length + _body.Length + 4));
+            PutPacket(_header);
+            PutPacket(_body);
+
+            return base.GetBytes();
+        }
+
+        private class Header : Packet
+        {
+            public Header(uint packetType, uint encryptType, byte[] ticket, string uin)
+            {
+                PutUintBE(packetType);
+                PutByte((byte)encryptType);
+
+                PutUintBE((uint)(ticket.Length + 4));
+                PutBytes(ticket);
+
+                PutByte(0x00);
+
+                PutUintBE((uint)(uin.Length + 4));
+                PutString(uin);
+            }
+        }
+
+        private class Body : Packet
+        {
+            public Body(Packet packet, ICryptor cryptor, byte[] cryptKey)
+            {
+                PutEncryptedPacket(packet, cryptor, cryptKey);
+            }
+        }
+    }
+}
