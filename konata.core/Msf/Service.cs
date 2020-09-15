eb664f0254e5269ae8c7d9580efc8d04ded559c3
TheSnowfield <thesnowfield@moezsoft.com>
2020-09-15 18:36:30 +0800

    code cleanup

--- konata.core/Msf/Service.cs
+++ konata.core/Msf/Service.cs
@@ -8,6 +8,6 @@ namespace Konata.Msf
 
         internal abstract bool Run(Core core, string method, params object[] args);
 
-        internal abstract bool Handle(Core core, byte[] data);
+        internal abstract bool Handle(Core core, params object[] args);
     }
 }
