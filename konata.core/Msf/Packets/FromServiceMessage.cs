d4978aa68b31f315d44e5ad3b5f2568ef31485bd
TheSnowfield <thesnowfield@moezsoft.com>
2020-09-16 23:49:53 +0800

    rename FromServiceMsg to FromServiceMessage

--- konata.core/Msf/Packets/FromServiceMessage.cs
+++ konata.core/Msf/Packets/FromServiceMessage.cs
@@ -2,7 +2,7 @@
 
 namespace Konata.Msf.Packets
 {
-    public class FromServiceMsg : Packet
+    public class FromServiceMessage : Packet
     {
         public readonly uint _length;
         public readonly uint _packetType;
@@ -10,7 +10,7 @@ namespace Konata.Msf.Packets
         public readonly byte _unknownZero;
         public readonly string _uinString;
 
-        public FromServiceMsg(byte[] data)
+        public FromServiceMessage(byte[] data)
         {
 
         }
