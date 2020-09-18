9df4d0c0a184dfb2bd377352bb317abf553bc09c
TheSnowfield <thesnowfield@moezsoft.com>
2020-09-19 00:12:58 +0800

    update service framework

--- Konata.Core/Msf/Services/JsApiSvr/WebView/WhiteList.cs
+++ Konata.Core/Msf/Services/JsApiSvr/WebView/WhiteList.cs
@@ -1,4 +1,27 @@
+using System;
+
 namespace Konata.Msf.Services.JsApiSvr.WebView
 {
+    internal class WhiteList : Service
+    {
+        private WhiteList()
+        {
+            Register("JsApiSvr.webview.whitelist", this);
+        }
 
+        public static Service Instance { get; } = new WhiteList();
+
+        protected override bool OnRun(Core core, string method, params object[] args)
+        {
+
+            return false;
+        }
+
+        protected override bool OnHandle(Core core, params object[] args)
+        {
+
+            return false;
+        }
+
+    }
 }
