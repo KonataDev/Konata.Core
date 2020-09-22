4294dfa3ec2861f8c972936ecd88f93c6730c068
TheSnowfield <thesnowfield@moezsoft.com>
2020-09-22 11:55:52 +0800

    fix tlv1 and tlv100

--- Konata.Core/Msf/Packets/Tlv/T100.cs
+++ Konata.Core/Msf/Packets/Tlv/T100.cs
@@ -4,36 +4,57 @@ namespace Konata.Msf.Packets.Tlv
 {
     public class T100 : TlvBase
     {
-        private const ushort _dbBufVer = 1;
-        private const int _ssoVer = 6;
-        private const int _sigmap = 34869472;
+        public T100(uint appId, uint subAppId, uint appClientVersion)
+            : base(0x0100, new T100Body(appId, subAppId, appClientVersion))
+        {
 
-        private readonly uint _appId;
-        private readonly uint _subAppId;
-        private readonly uint _appClientVersion;
+        }
 
-        public T100(uint appId, uint subAppId, uint appClientVersion) : base()
+        public T100(byte[] data)
+            : base(data)
+        {
+            _tlvBody = new T100Body(TakeAllBytes(out byte[] _));
+        }
+    }
+
+    public class T100Body : TlvBody
+    {
+        public readonly uint _ssoVer;
+        public readonly uint _sigmap;
+        public readonly ushort _dbBufVer;
+
+        public readonly uint _appId;
+        public readonly uint _subAppId;
+        public readonly uint _appClientVersion;
+
+        public T100Body(uint appId, uint subAppId, uint appClientVersion)
+        : base()
         {
             _appId = appId;
             _subAppId = subAppId;
             _appClientVersion = appClientVersion;
 
-            PackGeneric();
-        }
+            _ssoVer = 6;
+            _dbBufVer = 1;
+            _sigmap = 34869472;
 
-        public override void PutTlvCmd()
-        {
-            PutUshortBE(0x100);
-        }
-
-        public override void PutTlvBody()
-        {
             PutUshortBE(_dbBufVer);
             PutUintBE(_ssoVer);
             PutUintBE(_appId);
             PutUintBE(_subAppId);
             PutUintBE(_appClientVersion);
             PutUintBE(_sigmap);
+        }
+
+        public T100Body(byte[] data)
+            : base()
+        {
+            TakeUshortBE(out _dbBufVer);
+            TakeUintBE(out _ssoVer);
+            TakeUintBE(out _appId);
+            TakeUintBE(out _subAppId);
+            TakeUintBE(out _appClientVersion);
+            TakeUintBE(out _sigmap);
         }
     }
 }
