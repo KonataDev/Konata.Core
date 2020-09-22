82163ed6d8fa41dc4cb806536b7afcfa0a12e1b2
TheSnowfield <thesnowfield@moezsoft.com>
2020-09-23 01:57:55 +0800

    fix tlv52d

--- Konata.Core/Msf/Packets/Tlv/T52d.cs
+++ Konata.Core/Msf/Packets/Tlv/T52d.cs
@@ -1,6 +1,5 @@
 ﻿using System.IO;
 using System.Text;
-using Konata.Utils;
 using Konata.Msf.Packets.Protobuf;
 using ProtoBuf;
 
@@ -8,17 +7,39 @@ namespace Konata.Msf.Packets.Tlv
 {
     public class T52d : TlvBase
     {
-        private readonly byte[] _deviceReportInfo;
+        public T52d(byte[] deviceReportInfo)
+            : base(0x052d, new T52dBody(deviceReportInfo, deviceReportInfo.Length))
+        {
 
-        public T52d(byte[] deviceReportInfo) : base()
+        }
+
+        public T52d(string bootLoader, string version, string codeName,
+            string incremental, string fingerprint, string bootId, string androidId,
+            string baseBand, string innerVersion)
+
+            : base(0x052d, new T52dBody(bootLoader, version, codeName,
+             incremental, fingerprint, bootId, androidId, baseBand, innerVersion))
+        {
+
+        }
+    }
+
+    public class T52dBody : TlvBody
+    {
+        public readonly byte[] _deviceReportInfo;
+
+        public T52dBody(byte[] deviceReportInfo, int deviceReportInfoLength)
+            : base()
         {
             _deviceReportInfo = deviceReportInfo;
 
-            PackGeneric();
+            PutBytes(_deviceReportInfo);
         }
 
-        public T52d(string bootLoader, string version, string codeName, string incremental,
-            string fingerprint, string bootId, string androidId, string baseBand, string innerVersion) : base()
+        public T52dBody(string bootLoader, string version, string codeName,
+            string incremental, string fingerprint, string bootId, string androidId,
+            string baseBand, string innerVersion)
+            : base()
         {
             DeviceReport report = new DeviceReport
             {
@@ -33,22 +54,18 @@ namespace Konata.Msf.Packets.Tlv
                 InnerVersion = Encoding.UTF8.GetBytes(innerVersion)
             };
 
-            MemoryStream stream = new MemoryStream();
+            var stream = new MemoryStream();
             Serializer.Serialize(stream, report);
 
             _deviceReportInfo = stream.ToArray();
 
-            PackGeneric();
-        }
-
-        public override void PutTlvCmd()
-        {
-            PutUshortBE(0x52d);
-        }
-
-        public override void PutTlvBody()
-        {
             PutBytes(_deviceReportInfo);
+        }
+
+        public T52dBody(byte[] data)
+            : base(data)
+        {
+            TakeBytes(out _deviceReportInfo, Prefix.None);
         }
     }
 }
