1ab820a2c5731b8c5ebaa3229e685913bb132286
TheSnowfield <thesnowfield@moezsoft.com>
2020-09-23 00:42:43 +0800

    fix tlv177

--- Konata.Core/Msf/Packets/Tlv/T177.cs
+++ Konata.Core/Msf/Packets/Tlv/T177.cs
@@ -1,30 +1,38 @@
-﻿using Konata.Utils;
+﻿using System;
 
 namespace Konata.Msf.Packets.Tlv
 {
     public class T177 : TlvBase
     {
-        private readonly uint _buildTime;
-        private readonly string _sdkVersion;
+        public T177(uint buildTime, string sdkVersion)
+            : base(0x0177, new T177Body(buildTime, sdkVersion))
+        {
 
-        public T177(uint buildTime, string sdkVersion) : base()
+        }
+    }
+
+    public class T177Body : TlvBody
+    {
+        public readonly uint _buildTime;
+        public readonly string _sdkVersion;
+
+        public T177Body(uint buildTime, string sdkVersion)
+            : base()
         {
             _buildTime = buildTime;
             _sdkVersion = sdkVersion;
 
-            PackGeneric();
-        }
-
-        public override void PutTlvCmd()
-        {
-            PutUshortBE(0x177);
-        }
-
-        public override void PutTlvBody()
-        {
             PutByte(1);
             PutUintBE(_buildTime);
             PutString(_sdkVersion, 2);
+        }
+
+        public T177Body(byte[] data)
+            : base(data)
+        {
+            EatBytes(1);
+            TakeUintBE(out _buildTime);
+            TakeString(out _sdkVersion, Prefix.Uint16);
         }
     }
 }
