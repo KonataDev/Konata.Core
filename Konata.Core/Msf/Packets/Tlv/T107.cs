67e074e9f167419e49e048b38bb324a7c72a8525
TheSnowfield <thesnowfield@moezsoft.com>
2020-09-22 15:54:04 +0800

    fix tlv107

--- Konata.Core/Msf/Packets/Tlv/T107.cs
+++ Konata.Core/Msf/Packets/Tlv/T107.cs
@@ -1,35 +1,44 @@
-﻿using Konata.Utils;
+﻿using System;
 
 namespace Konata.Msf.Packets.Tlv
 {
     public class T107 : TlvBase
     {
-        private readonly ushort _picType;
-        private readonly byte _capType;
-        private readonly ushort _picSize;
-        private readonly byte _retType;
-
-        public T107(int picType, int capType = 0, int picSize = 0, int retType = 1) : base()
+        public T107(ushort picType, byte capType = 0, ushort picSize = 0, byte retType = 1)
+            : base(0x0107, new T107Body(picType, capType, picSize, retType))
         {
-            _picType = (ushort)picType;
-            _capType = (byte)capType;
-            _picSize = (ushort)picSize;
-            _retType = (byte)retType;
 
-            PackGeneric();
         }
+    }
 
-        public override void PutTlvCmd()
-        {
-            PutUshortBE(0x107);
-        }
+    public class T107Body : TlvBody
+    {
+        public readonly ushort _picType;
+        public readonly byte _capType;
+        public readonly ushort _picSize;
+        public readonly byte _retType;
 
-        public override void PutTlvBody()
+        public T107Body(ushort picType, byte capType, ushort picSize, byte retType)
+            : base()
         {
+            _picType = picType;
+            _capType = capType;
+            _picSize = picSize;
+            _retType = retType;
+
             PutUshortBE(_picType);
             PutByte(_capType);
             PutUshortBE(_picSize);
             PutByte(_retType);
+        }
+
+        public T107Body(byte[] data)
+            : base(data)
+        {
+            TakeUshortBE(out _picType);
+            TakeByte(out _capType);
+            TakeUshortBE(out _picSize);
+            TakeByte(out _retType);
         }
     }
 }
