4eaefc0b3f031b164ab4c663057a17c4d8aa1072
TheSnowfield <thesnowfield@moezsoft.com>
2020-09-23 01:41:14 +0800

    fix tlv511

--- Konata.Core/Msf/Packets/Tlv/T511.cs
+++ Konata.Core/Msf/Packets/Tlv/T511.cs
@@ -1,30 +1,47 @@
-﻿using Konata.Utils;
+﻿using System;
 
 namespace Konata.Msf.Packets.Tlv
 {
     public class T511 : TlvBase
     {
-        private readonly string[] _domains;
+        public T511(string[] domains)
+            : base(0x0511, new T511Body(domains))
+        {
 
-        public T511(string[] domains) : base()
+        }
+    }
+
+    public class T511Body : TlvBody
+    {
+        public readonly string[] _domains;
+
+        public T511Body(string[] domains)
+            : base()
         {
             _domains = domains;
 
-            PackGeneric();
-        }
-
-        public override void PutTlvCmd()
-        {
-            PutUshortBE(0x0511);
-        }
-
-        public override void PutTlvBody()
-        {
             PutUshortBE((ushort)_domains.Length);
-            foreach (string element in _domains)
             {
-                PutByte(0x01);
-                PutString(element, 2);
+                foreach (string element in _domains)
+                {
+                    PutByte(0x01);
+                    PutString(element, 2);
+                }
+            }
+        }
+
+        public T511Body(byte[] data)
+           : base(data)
+        {
+            TakeUshortBE(out var length);
+            
+            _domains = new string[length];
+            {
+                for (int i = 0; i < length; ++i)
+                {
+                    EatBytes(1);
+                    TakeString(out _domains[i], Prefix.Uint16);
+                }
             }
         }
     }
