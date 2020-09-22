fb1333f69daf6e9222c99449b7f0882783cb98c2
TheSnowfield <thesnowfield@moezsoft.com>
2020-09-23 01:07:50 +0800

    fix tlv192

--- Konata.Core/Msf/Packets/Tlv/T192.cs
+++ Konata.Core/Msf/Packets/Tlv/T192.cs
@@ -1,26 +1,32 @@
-﻿using Konata.Utils;
+﻿using System;
 
 namespace Konata.Msf.Packets.Tlv
 {
     public class T192 : TlvBase
     {
+        public T192(string url)
+            : base(0x0192, new T192Body(url))
+        {
+
+        }
+    }
+
+    public class T192Body : TlvBody
+    {
         private readonly string _url;
 
-        public T192(string url) : base()
+        public T192Body(string url)
+            : base()
         {
             _url = url;
 
-            PackGeneric();
-        }
-
-        public override void PutTlvCmd()
-        {
-            PutUshortBE(0x192);
-        }
-
-        public override void PutTlvBody()
-        {
             PutString(_url, 2);
+        }
+
+        public T192Body(byte[] data)
+            : base(data)
+        {
+            TakeString(out _url, Prefix.Uint16);
         }
     }
 }
