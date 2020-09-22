1878ffdd72b1cfef96eb327c2007dbe8ea11725e
TheSnowfield <thesnowfield@moezsoft.com>
2020-09-22 16:47:10 +0800

    fix tlv128

--- Konata.Core/Msf/Packets/Tlv/T128.cs
+++ Konata.Core/Msf/Packets/Tlv/T128.cs
@@ -1,19 +1,32 @@
-﻿using Konata.Utils;
+﻿using System;
 
 namespace Konata.Msf.Packets.Tlv
 {
     public class T128 : TlvBase
     {
-        private readonly bool _isNewInstall;
-        private readonly bool _isGuidAvaliable;
-        private readonly bool _isGuidChanged;
-        private readonly byte[] _guid;
-        private readonly uint _guidFlag;
-        private readonly string _deviceModel;
-        private readonly string _deviceBrand;
-
         public T128(bool isNewInstall, bool isGuidAvaliable, bool isGuidChanged,
-            byte[] guid, uint guidFlag, string deviceModel, string deviceBrand) : base()
+            byte[] guid, uint guidFlag, string deviceModel, string deviceBrand)
+
+            : base(0x0128, new T128Body(isNewInstall, isGuidAvaliable, isGuidChanged,
+             guid, guidFlag, deviceModel, deviceBrand))
+        {
+
+        }
+    }
+
+    public class T128Body : TlvBody
+    {
+        public readonly bool _isNewInstall;
+        public readonly bool _isGuidAvaliable;
+        public readonly bool _isGuidChanged;
+        public readonly byte[] _guid;
+        public readonly uint _guidFlag;
+        public readonly string _deviceModel;
+        public readonly string _deviceBrand;
+
+        public T128Body(bool isNewInstall, bool isGuidAvaliable, bool isGuidChanged,
+            byte[] guid, uint guidFlag, string deviceModel, string deviceBrand)
+            : base()
         {
             _isNewInstall = isNewInstall;
             _isGuidAvaliable = isGuidAvaliable;
@@ -23,24 +36,27 @@ namespace Konata.Msf.Packets.Tlv
             _deviceModel = deviceModel;
             _deviceBrand = deviceBrand;
 
-            PackGeneric();
-        }
-
-        public override void PutTlvCmd()
-        {
-            PutUshortBE(0x128);
-        }
-
-        public override void PutTlvBody()
-        {
             PutUshortBE(0);
-            PutBoolBE(_isNewInstall,1);
+            PutBoolBE(_isNewInstall, 1);
             PutBoolBE(_isGuidAvaliable, 1);
             PutBoolBE(_isGuidChanged, 1);
             PutUintBE(_guidFlag);
-            PutString(_deviceModel,2, 32);
+            PutString(_deviceModel, 2, 32);
             PutBytes(_guid, 2, 16);
             PutString(_deviceBrand, 2, 16);
+        }
+
+        public T128Body(byte[] data)
+            : base(data)
+        {
+            EatBytes(2);
+            TakeBoolBE(out _isNewInstall, 1);
+            TakeBoolBE(out _isGuidAvaliable, 1);
+            TakeBoolBE(out _isGuidChanged, 1);
+            TakeUintBE(out _guidFlag);
+            TakeString(out _deviceModel, Prefix.Uint16);
+            TakeBytes(out _guid, Prefix.Uint16);
+            TakeString(out _deviceBrand, Prefix.Uint16);
         }
     }
 }
