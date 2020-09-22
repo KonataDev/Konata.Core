c721d3cd55f09d04fdf9f4a3e3012dcf673f5895
TheSnowfield <thesnowfield@moezsoft.com>
2020-09-22 16:27:36 +0800

    fix tlv116

--- Konata.Core/Msf/Packets/Tlv/T116.cs
+++ Konata.Core/Msf/Packets/Tlv/T116.cs
@@ -1,46 +1,63 @@
-﻿using Konata.Utils;
+﻿using System;
 
 namespace Konata.Msf.Packets.Tlv
 {
     public class T116 : TlvBase
     {
-        private const byte _ver = 0;
-
-        private readonly uint _bitmap;
-        private readonly uint _getSig;
-        private readonly long[] _subAppIdList;
-
-        public T116(int bitmap, int getSig, long[] subAppIdList = null) : base()
+        public T116(uint bitmap, uint getSig, uint[] subAppIdList = null)
+            : base(0x0116, new T116Body(bitmap, getSig, subAppIdList))
         {
+
+        }
+    }
+
+    public class T116Body : TlvBody
+    {
+        public readonly byte _ver;
+        public readonly uint _bitmap;
+        public readonly uint _getSig;
+        public readonly uint[] _subAppIdList;
+
+        public T116Body(uint bitmap, uint getSig, uint[] subAppIdList = null)
+            : base()
+        {
+            _ver = 0;
+            _bitmap = bitmap;
+            _getSig = getSig;
+
             if (subAppIdList == null)
             {
-                _subAppIdList = new long[] { 1600000226L };
+                _subAppIdList = new uint[] { 1600000226U };
             }
 
-            _bitmap = (uint)bitmap;
-            _getSig = (uint)getSig;
-
-            PackGeneric();
-        }
-
-        public override void PutTlvCmd()
-        {
-            PutUshortBE(0x116);
-        }
-
-        public override void PutTlvBody()
-        {
-            StreamBuilder builder = new StreamBuilder();
-            builder.PutByte(_ver);
-            builder.PutUintBE(_bitmap);
-            builder.PutUintBE(_getSig);
-            builder.PutByte((byte)_subAppIdList.Length);
-            foreach (long element in _subAppIdList)
+            PutByte(_ver);
+            PutUintBE(_bitmap);
+            PutUintBE(_getSig);
+            
+            PutByte((byte)_subAppIdList.Length);
             {
-                builder.PutUintBE((uint)element);
+                foreach (uint element in _subAppIdList)
+                {
+                    PutUintBE(element);
+                }
             }
+        }
 
-            PutBytes(builder.GetBytes());
+        public T116Body(byte[] data)
+            : base(data)
+        {
+            TakeByte(out _ver);
+            TakeUintBE(out _bitmap);
+            TakeUintBE(out _getSig);
+            
+            TakeByte(out var count);
+            {
+                _subAppIdList = new uint[count];
+                for (int i = 0; i < count; ++i)
+                {
+                    TakeUintBE(out _subAppIdList[i]);
+                }
+            }
         }
     }
 }
