bfa515e3a187ccf462a6d917c9ac61c8e33a81d8
TheSnowfield <thesnowfield@moezsoft.com>
2020-09-24 13:40:14 +0800

    add OicqRequestCheckImage

--- Konata.Core/Msf/Packets/Oicq/OicqRequestCheckImage.cs
+++ Konata.Core/Msf/Packets/Oicq/OicqRequestCheckImage.cs
@@ -1,23 +1,42 @@
 ﻿using System;
 using Konata.Msf.Crypto;
+using Konata.Msf.Packets.Tlv;
 
 namespace Konata.Msf.Packets.Oicq
 {
+    using Tlv = Tlv.Tlv;
+
     public class OicqRequestCheckImage : OicqRequest
     {
-        public OicqRequestCheckImage(uint uin, KeyRing keyring)
-            : base(0x0810, 0x02, uin, new XCaptcha(), 
+        private const ushort OicqCommand = 0x0810;
+        private const ushort OicqSubCommand = 0x0002;
+
+        public OicqRequestCheckImage(uint uin, KeyRing keyring,
+            string sigSission, string sigTicket)
+
+            : base(OicqCommand, OicqSubCommand, uin,
+                  new XCaptcha(sigSission, sigTicket),
                   keyring._shareKey, keyring._randKey, keyring._defaultPublicKey)
         {
 
         }
-    }
 
-    public class XCaptcha : OicqRequestBody
-    {
-        public XCaptcha()
+        public class XCaptcha : OicqRequestBody
         {
+            public XCaptcha(string sigSission, string sigTicket)
+                : base()
+            {
+                TlvPacker tlvs = new TlvPacker();
+                {
+                    tlvs.PutTlv(new Tlv(0x0193, new T193Body(sigTicket)));
+                    tlvs.PutTlv(new Tlv(0x0008, new T8Body()));
+                    tlvs.PutTlv(new Tlv(0x0104, new T104Body(sigSission)));
+                    tlvs.PutTlv(new Tlv(0x0116, new T116Body(150470524, 66560)));
+                }
 
+                PutUshortBE(OicqSubCommand);
+                PutBytes(tlvs.GetBytes(true));
+            }
         }
     }
 }
