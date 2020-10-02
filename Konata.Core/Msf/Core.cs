b0c2a044cc68c2d2106111602499350d735fa052
TheSnowfield <thesnowfield@moezsoft.com>
2020-10-02 20:20:09 +0800

    add comment; code cleanup

--- Konata.Core/Msf/Core.cs
+++ Konata.Core/Msf/Core.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Threading;
 using Konata.Msf;
 using Konata.Msf.Crypto;
 using Konata.Msf.Network;
@@ -18,8 +17,6 @@ namespace Konata.Msf
     //  ||	  |   /    \
     //  ++--- WtLogin OnlinePush...etc 
     //   +----------------+
-
-    //public delegate void EventDelegate(uint signal);
 
     public class Core
     {
@@ -41,40 +38,89 @@ namespace Konata.Msf
             _keyRing = new KeyRing(uin, password);
         }
 
+        #region Core Methods
+
+        /// <summary>
+        /// 連接到伺服器
+        /// </summary>
+        /// <returns></returns>
         public bool Connect()
         {
-            return _ssoMan.Initialize();
+            return _ssoMan.Connect();
         }
 
+        /// <summary>
+        /// 斷開連接
+        /// </summary>
+        /// <returns></returns>
+        public bool DisConnect()
+        {
+            return _ssoMan.Disconnect();
+        }
+
+        /// <summary>
+        /// 請求登錄
+        /// </summary>
+        /// <returns></returns>
         public bool WtLoginTgtgt()
         {
             return Service.Run(this, "Wtlogin.Login", "Request_TGTGT");
         }
 
+        /// <summary>
+        /// 提交滑塊驗證碼
+        /// </summary>
+        /// <param name="sigSission"></param>
+        /// <param name="sigTicket"></param>
+        /// <returns></returns>
         public bool WtLoginCheckSlider(string sigSission, string sigTicket)
         {
             return Service.Run(this, "Wtlogin.Login", "Request_SliderCaptcha",
                 sigSission, sigTicket);
         }
 
+        #endregion
+
+        #region Event Metods
+
+        /// <summary>
+        /// 發送用戶事件. 該事件會被派發到用戶層
+        /// </summary>
+        /// <param name="type"></param>
+        /// <param name="args"></param>
         public void PostUserEvent(EventType type, params object[] args)
         {
             _bot.PostUserEvent(type, args);
         }
 
-        public void PostSystemEvent(EventType type, params object[] args)
-        {
-            _bot.PostSystemEvent(type, args);
-        }
-        
+        /// <summary>
+        /// 發送用戶事件. 該事件會被派發到用戶層
+        /// </summary>
+        /// <param name="type"></param>
         public void PostUserEvent(EventType type)
         {
             _bot.PostUserEvent(type, null);
         }
-        
+
+        /// <summary>
+        /// 發送系統事件. 該事件會在系統内部響應
+        /// </summary>
+        /// <param name="type"></param>
+        /// <param name="args"></param>
+        public void PostSystemEvent(EventType type, params object[] args)
+        {
+            _bot.PostSystemEvent(type, args);
+        }
+
+        /// <summary>
+        /// 發送系統事件. 該事件會在系統内部響應
+        /// </summary>
+        /// <param name="type"></param>
         public void PostSystemEvent(EventType type)
         {
             _bot.PostSystemEvent(type, null);
         }
+
+        #endregion 
     }
 }
