d3b79667a28290d9fcd6e4b932e70caf84185bf9
TheSnowfield <thesnowfield@moezsoft.com>
2020-10-09 23:30:09 +0800

    add StatSvc.register

---
+++ Konata.Core/Msf/Services/StatSvc/Register.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Konata.Msf.Services.Wtlogin
+{
+    internal class Register : Service
+    {
+        private Register()
+        {
+            Register("StatSvc.register", this);
+        }
+
+        public static Service Instance { get; } = new Register();
+
+        protected override bool OnRun(Core core, string method, params object[] args)
+        {
+            switch (method)
+            {
+                default: return false;
+            }
+        }
+
+        protected override bool OnHandle(Core core, params object[] args)
+        {
+            if (args == null || args.Length == 0)
+                return false;
+
+            return false;
+        }
+    }
+}
