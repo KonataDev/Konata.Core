7949827966a5ae92f423b95e4a758cb186f1924e
TheSnowfield <thesnowfield@moezsoft.com>
2020-10-10 19:59:46 +0800

    add test

---
+++ Konata.Test/Test.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Konata.Test
+{
+    public abstract class Test
+    {
+        public Test() { }
+
+        public abstract bool Run();
+    }
+}
---
+++ Konata.Test/Tests/TestJceWriter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Konata.Utils;
+
+namespace Konata.Test.Tests
+{
+    public class TestJceWriter : Test
+    {
+        public override bool Run()
+        {
+            var jce = new JceWriter();
+            jce.PutJceByte(0, 1);
+            jce.PutJceShort(1, 2);
+            jce.PutJceInt(2, 3);
+            jce.PutJceLong(3, 4);
+            {
+                var jce2 = new JceWriter();
+                jce2.PutJceString4(0, "Hello JCE");
+                jce2.PutJceInt(1, 233);
+
+                jce.PutJceWriter(4, jce2);
+            }
+            jce.PutJceLong(5, 4);
+
+            return true;
+        }
+    }
+}
