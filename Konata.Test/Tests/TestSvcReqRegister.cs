952bea1462998bfbccdeea3274418b684c19f2f8
TheSnowfield <thesnowfield@moezsoft.com>
2020-10-11 20:25:28 +0800

    update tests

--- Konata.Test/Tests/TestSvcReqRegister.cs
+++ Konata.Test/Tests/TestSvcReqRegister.cs
@@ -8,53 +8,56 @@ namespace Konata.Test.Tests
     {
         public override bool Run()
         {
-            var request = new SvcReqRegister();
-            request._uin = 3322047216;
-            request._bid = 7;
-            request._connType = 0;
-            request._other = "";
-            request._status = 11;
-            request._onlinePush = 0;
-            request._isOnline = 0;
-            request._isShowOnline = 0;
-            request._kikPC = 0;
-            request._kikWeak = 0;
-            request._timeStamp = 56;
-            request._osVersion = 27;
-            request._netType = 1;
-            request._regType = 0;
-            request._guid = new byte[]
+            var requestBody = new SvcReqRegister.XSvcRegister()
             {
-                0x1D, 0xA5, 0xA0, 0xBC,
-                0x7E, 0x76, 0x75, 0x7D,
-                0x54, 0x71, 0x3A, 0xD9,
-                0x17, 0x5E, 0xF8, 0xDA
+                _uin = 2842875712,
+                _bid = 7,
+                _connType = 0,
+                _other = "",
+                _status = 11,
+                _onlinePush = 0,
+                _isOnline = 0,
+                _isShowOnline = 0,
+                _kikPC = 0,
+                _kikWeak = 0,
+                _timeStamp = 56,
+                _osVersion = 27,
+                _netType = 1,
+                _regType = 0,
+                _guid = new byte[]
+                {
+                    0x1D, 0xA5, 0xA0, 0xBC,
+                    0x7E, 0x76, 0x75, 0x7D,
+                    0x54, 0x71, 0x3A, 0xD9,
+                    0x17, 0x5E, 0xF8, 0xDA
+                },
+                _localeID = 2052,
+                _slientPush = 0,
+                _devName = "SM-G9009W",
+                _devType = "SM-G9009W",
+                _osVer = "8.1.0",
+                _openPush = 1,
+                _largeSeq = 99,
+                _oldSSOIp = 0,
+                _newSSOIp = 2081292189,
+                _channelNo = "",
+                _cpId = 0,
+                _vendorName = "SAMSUNG",
+                _vendorOSName = "klteduosctc-user 6.0",
+                _osIdfa = "",
+                _cmd0x769Reqbody = new byte[]
+                {
+                    0x0a, 0x08, 0x08, 0x2E,
+                    0x10, 0x9A, 0xEF, 0x9C,
+                    0xFB, 0x05, 0x0A, 0x05,
+                    0x08, 0x9B, 0x02, 0x10, 0x00
+                },
+                _isSetStatus = 0,
+                _extOnlineStatus = 0,
+                _batteryStatus = 0
             };
-            request._localeID = 2052;
-            request._slientPush = 0;
-            request._devName = "SM-G9009W";
-            request._devType = "SM-G9009W";
-            request._osVer = "8.1.0";
-            request._openPush = 1;
-            request._largeSeq = 99;
-            request._oldSSOIp = 0;
-            request._newSSOIp = 2081292189;
-            request._channelNo = "";
-            request._cpId = 0;
-            request._vendorName = "SAMSUNG";
-            request._vendorOSName = "klteduosctc-user 6.0";
-            request._osIdfa = "";
-            request._cmd0x769Reqbody = new byte[] {
-                0x0a, 0x08, 0x08, 0x2E,
-                0x10, 0x9A, 0xEF, 0x9C,
-                0xFB, 0x05, 0x0A, 0x05,
-                0x08, 0x9B, 0x02, 0x10, 0x00
-            };
-            request._isSetStatus = 0;
-            request._extOnlineStatus = 0;
-            request._batteryStatus = 0;
 
-            request.WriteTo();
+            var request = new SvcReqRegister(0, 0, 0, 0, requestBody.Encode());
             Print(request.ToString());
 
             return true;
