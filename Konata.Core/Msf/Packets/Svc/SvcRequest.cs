9d89e93b7cad5e5ad85a3ddc71cd83c1f7fb1aad
TheSnowfield <thesnowfield@moezsoft.com>
2020-10-11 20:22:52 +0800

    update svc

---
+++ Konata.Core/Msf/Packets/Svc/SvcRequest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using Konata.Msf.Packets.Wup;
+
+namespace Konata.Msf.Packets.Svc
+{
+    public class SvcRequest : UniPacket
+    {
+        public SvcRequest(string servantName, string funcName,
+            byte packetType, ushort messageType, ushort requestId, ushort oldRespIret,
+            SvcRequestBody svcRequest)
+
+            : base(true, servantName, funcName, packetType, messageType,
+                  requestId, oldRespIret, new SvcRequestBody(funcName, svcRequest))
+        {
+
+        }
+    }
+
+    public class SvcRequestBody : UniPacketBody
+    {
+        public SvcRequestBody()
+            : base()
+        {
+
+        }
+
+        public SvcRequestBody(string funcName, SvcRequestBody body)
+            : base()
+        {
+            var dict = new Dictionary<string, SvcRequestBody>();
+            dict.Add(funcName, body);
+            {
+                Write(dict, 0);
+            }
+        }
+    }
+}
