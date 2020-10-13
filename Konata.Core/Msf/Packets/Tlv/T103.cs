0ec3c5fc312c0d17927be85e30c12425b128fc93
TheSnowfield <thesnowfield@moezsoft.com>
2020-10-14 02:21:45 +0800

    add empty tlvs

---
+++ Konata.Core/Msf/Packets/Tlv/T103.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Konata.Msf.Packets.Tlv
+{
+    /// <summary>
+    /// TODO
+    /// </summary>
+    public class T103Body : TlvBody
+    {
+        public T103Body()
+            : base()
+        {
+        }
+
+        public T103Body(byte[] data)
+            : base(data)
+        {
+
+        }
+    }
+}
---
+++ Konata.Core/Msf/Packets/Tlv/T163.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Konata.Msf.Packets.Tlv
+{
+    /// <summary>
+    /// TODO
+    /// </summary>
+    public class T163Body : TlvBody
+    {
+        public T163Body()
+            : base()
+        {
+        }
+
+        public T163Body(byte[] data)
+            : base(data)
+        {
+
+        }
+    }
+}
