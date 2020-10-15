445de8e071fe2203a4e0fbffdd950d6d7e2b95c2
TheSnowfield <thesnowfield@moezsoft.com>
2020-10-15 08:15:20 +0800

    add MessageSvc.PushNotify

---
+++ Konata.Core/Msf/Services/MessageSvc/PushNotify.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Konata.Msf.Services.MessageSvc
+{
+    internal class PushNotify : Service
+    {
+        private PushNotify()
+        {
+            Register("MessageSvc.PushNotify", this);
+        }
+
+        public static Service Instance { get; } = new PushNotify();
+
+        protected override bool OnRun(Core core, string method, params object[] args)
+        {
+            if (method != "")
+                throw new Exception("???");
+
+            return false;
+        }
+
+        protected override bool OnHandle(Core core, params object[] args)
+        {
+            if (args == null || args.Length == 0)
+                return false;
+
+            return false;
+        }
+
+        private bool Handle_Notify(Core core)
+        {
+            // TODO: MessageSvc.PbGetMsg
+            return true;
+        }
+    }
+}
