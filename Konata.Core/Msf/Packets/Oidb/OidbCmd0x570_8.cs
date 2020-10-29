4579805c840d3697e67a0790e05b1730d52940f7
TheSnowfield <thesnowfield@moezsoft.com>
2020-10-30 06:35:43 +0800

    update PidbSSOPkg

---
+++ Konata.Core/Msf/Packets/Oidb/OidbCmd0x570_8.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Konata.Library.IO;
+
+namespace Konata.Msf.Packets.Oidb
+{
+    /// <summary>
+    /// 群禁言
+    /// </summary>
+
+    public class OidbCmd0x570_8 : OidbSSOPkg
+    {
+        public OidbCmd0x570_8(uint group, uint uin, uint sec)
+
+            : base(1392, 8, (ByteBuffer t) =>
+            {
+                t.PutUintBE(group);
+                t.PutHexString("20 00 01");
+                t.PutUintBE(uin);
+                t.PutUintBE(sec);
+            })
+        {
+
+        }
+    }
+}
--- Konata.Core/Msf/Packets/Oidb/OidbSSOPkg.cs
+++ Konata.Core/Msf/Packets/Oidb/OidbSSOPkg.cs
@@ -1,9 +1,71 @@
 ﻿using System;
+using Konata.Library.IO;
+using Konata.Library.Protobuf;
 
 namespace Konata.Msf.Packets.Oidb
 {
-    class OidbSSOPkg : Packet
-    {
+    public delegate void OidbPayloadWriter(ProtoTreeRoot writer);
+    public delegate void OidbPayloadReader(ProtoTreeRoot reader);
+    public delegate void OidbByteBufferWriter(ByteBuffer writer);
 
+    public class OidbSSOPkg : Packet
+    {
+        public readonly ProtoTreeRoot root;
+        public readonly int svcCmd;
+        public readonly int svcType;
+        public readonly int svcResult;
+        public readonly string errorMsg;
+        public readonly string clientVer;
+
+        public OidbSSOPkg(int cmd, int type, OidbPayloadWriter writer)
+        {
+            svcCmd = cmd;
+            svcType = type;
+
+            root = new ProtoTreeRoot();
+            {
+                root.addLeafVar("08", svcCmd);
+                root.addLeafVar("10", svcType);
+                root.addLeafVar("18", svcResult);
+
+                var payload = new ProtoTreeRoot();
+                {
+                    writer(payload);
+                }
+                root.addLeafByteBuffer("22", payload.Serialize());
+
+                root.addLeafString("28", errorMsg);
+                root.addLeafString("30", clientVer);
+            }
+            PutByteBuffer(root.Serialize());
+        }
+
+        public OidbSSOPkg(int cmd, int type, OidbByteBufferWriter writer)
+        {
+            svcCmd = cmd;
+            svcType = type;
+
+            root = new ProtoTreeRoot();
+            {
+                root.addLeafVar("08", svcCmd);
+                root.addLeafVar("10", svcType);
+                root.addLeafVar("18", svcResult);
+
+                var payload = new ByteBuffer();
+                {
+                    writer(payload);
+                }
+                root.addLeafByteBuffer("22", payload);
+
+                root.addLeafString("28", errorMsg);
+                root.addLeafString("30", clientVer);
+            }
+            PutByteBuffer(root.Serialize());
+        }
+
+        public OidbSSOPkg(byte[] data)
+        {
+
+        }
     }
 }
