d36eab3c18c694e663c3309680d08ce271379847
TheSnowfield <thesnowfield@moezsoft.com>
2020-10-30 07:02:28 +0800

    add test

---
+++ Konata.Test/Tests/TestProtobufMsgWithDraw.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Konata.Msf.Packets.Protobuf;
+
+namespace Konata.Test.Tests
+{
+    class TestProtobufMsgWithDraw : Test
+    {
+        public override bool Run()
+        {
+            var packet = new ProtoMsgWithDraw(23333333, 1567772123);
+            Print(packet.Serialize());
+
+            return true;
+        }
+    }
+}
