7a8d35a0850696f11aa07e731d1245d397b7314a
TheSnowfield <thesnowfield@moezsoft.com>
2020-10-30 06:35:55 +0800

    add test

---
+++ Konata.Test/Tests/TestOidbCmd0x570_8.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Konata.Msf.Packets.Oidb;
+
+namespace Konata.Test.Tests
+{
+    class TestOidbCmd0x570_8 : Test
+    {
+        public override bool Run()
+        {
+            var oidb = new OidbCmd0x570_8(2333333, 12345678, 600);
+            Print(oidb);
+
+            return true;
+        }
+    }
+}
