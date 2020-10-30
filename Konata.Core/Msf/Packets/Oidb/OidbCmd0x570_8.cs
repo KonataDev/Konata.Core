ebaeec8421c45251cdeeee39873cbef4cd221f08
TheSnowfield <thesnowfield@moezsoft.com>
2020-10-30 21:20:22 +0800

    code cleanup

--- Konata.Core/Msf/Packets/Oidb/OidbCmd0x570_8.cs
+++ Konata.Core/Msf/Packets/Oidb/OidbCmd0x570_8.cs
@@ -11,7 +11,7 @@ namespace Konata.Msf.Packets.Oidb
     {
         public OidbCmd0x570_8(uint group, uint uin, uint sec)
 
-            : base(1392, 8, (ByteBuffer t) =>
+            : base(0x570, 0x08, (ByteBuffer t) =>
             {
                 t.PutUintBE(group);
                 t.PutHexString("20 00 01");
---
+++ Konata.Core/Msf/Packets/Oidb/OidbCmd0x899_0.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Konata.Library.Protobuf;
+
+namespace Konata.Msf.Packets.Oidb
+{
+    /// <summary>
+    /// 拉取群管理列表
+    /// 或許可以有更大的用途
+    /// </summary>
+
+    public class OidbCmd0x899_0 : OidbSSOPkg
+    {
+        public OidbCmd0x899_0(uint group)
+            : base(0x899, 0x00, (ProtoTreeRoot t) =>
+            {
+                t.addLeafVar("08", group); // uint64_group_code
+                t.addLeafVar("10", 0);     // uint64_start_uin
+                t.addLeafVar("18", 2);     // uint32_identify_flag
+
+                //  8 uint64_group_code
+                // 16 uint64_start_uin
+                // 24 uint32_identify_flag
+                // 32 rpt_uint64_uin_list
+                // 42 memberlist_opt
+                // 48 uint32_member_num
+                // 56 uint32_filter_method
+                // 64 uint32_online_flag
+
+                t.addTree("2A", (ProtoTreeRoot memberlist) =>
+                {
+                    memberlist.addLeafVar("08", 0);   // uint64_member_uin
+                    memberlist.addLeafVar("9001", 1); // uint32_privilege
+
+                    //   8 uint64_member_uin
+                    //  16 uint32_uin_flag
+                    //  24 uint32_uin_flagex
+                    //  32 uint32_uin_mobile_flag
+                    //  40 uint32_uin_arch_flag
+                    //  48 uint32_join_time
+                    //  56 uint32_old_msg_seq
+                    //  64 uint32_new_msg_seq
+                    //  72 uint32_last_speak_time
+                    //  80 uint32_level
+                    //  88 uint32_point
+                    //  96 uint32_shutup_timestap
+                    // 104 uint32_flagex2
+                    // 114 bytes_special_title
+                    // 120 uint32_special_title_expire_time
+                    // 128 uint32_active_day
+                    // 138 bytes_uin_key
+                    // 144 uint32_privilege
+                    // 154 bytes_rich_info
+                });
+            })
+        {
+
+        }
+    }
+}
