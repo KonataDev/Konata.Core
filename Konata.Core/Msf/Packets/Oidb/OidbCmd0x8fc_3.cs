c5f2a8a219bc5f24d102516977168a45a4789b93
TheSnowfield <thesnowfield@moezsoft.com>
2020-11-01 01:51:32 +0800

    add Oidb 0x8fc_3

---
+++ Konata.Core/Msf/Packets/Oidb/OidbCmd0x8fc_3.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Konata.Msf.Packets.Oidb
+{
+    /// <summary>
+    /// 設置自身群名片
+    /// </summary>
+
+    public class OidbCmd0x8fc_3 : OidbCmd0x8fc
+    {
+        public OidbCmd0x8fc_3(uint groupUin, uint selfUin, string cardName)
+            : base(0x03, new ReqBody
+            {
+                group_code = groupUin,
+
+                rpt_mem_level_info = new MemberInfo
+                {
+                    uin = selfUin,
+                    member_card_name = cardName,
+                    // <TODO> comm_rich_card_name
+                },
+
+                msg_client_info = new ClientInfo
+                {
+                    implat = 109,
+                    clientver = "2" // getNumberOfCameras
+                }
+            })
+        {
+
+        }
+    }
+}
