058362bec418b0431a4234d1984c2eed39cf82c5
TheSnowfield <thesnowfield@moezsoft.com>
2020-11-01 02:02:27 +0800

    add new test; code cleanup

--- Konata.Test/Tests/TestOidbCmd0x8fc_2.cs
+++ Konata.Test/Tests/TestOidbCmd0x8fc_2.cs
@@ -3,11 +3,11 @@ using Konata.Msf.Packets.Oidb;
 
 namespace Konata.Test.Tests
 {
-    class TestOidbCmd0x88d_0 : Test
+    class TestOidbCmd0x8fc_2 : Test
     {
         public override bool Run()
         {
-            var packet = new OidbCmd0x88d_0(2333);
+            var packet = new OidbCmd0x8fc_2(2333, 23333, "咕咕");
             Print(packet);
 
             return true;
