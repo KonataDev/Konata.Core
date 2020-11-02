3d731bd1b6f527806d1908e81d1b98c6d5030373
TheSnowfield <thesnowfield@moezsoft.com>
2020-11-03 07:56:00 +0800

    Rewrite SsoMessage

--- Konata.Core/Msf/Packets/Sso/SsoMessage.cs
+++ Konata.Core/Msf/Packets/Sso/SsoMessage.cs
@@ -1,108 +1,101 @@
 ﻿using System;
 using Konata.Library.IO;
-using Konata.Msf;
-using Konata.Msf.Crypto;
 
-namespace Konata.Msf.Packets
+namespace Konata.Msf.Packets.Sso
 {
-    public class SsoMessage : Packet
-    {
-        public Header ssoHeader;
-        public ByteBuffer ssoWupBuffer;
 
-        public SsoMessage(uint sequence, uint session, string command,
-            byte[] tgtToken, ByteBuffer packet)
+    public enum RequestPktType : uint
+    {
+        TypeA = 0x0A,
+        TypeB = 0x0B
+    }
+
+    public delegate void SsoPayloadWriter(ByteBuffer writer);
+
+    public class SsoMessage
+    {
+        private uint ssoSequence;
+        private RequestPktType ssoPktType;
+        private ByteBuffer ssoPayload;
+        private string ssoCommand;
+        private uint ssoSession;
+
+        public SsoMessage(uint ssoseq, string ssoCmd, uint ssoSess,
+            RequestPktType type, SsoPayloadWriter writer)
             : base()
         {
-            ssoHeader = new Header(sequence, session, command, tgtToken);
-            ssoWupBuffer = packet;
+            ssoCommand = ssoCmd;
+            ssoSequence = ssoseq;
+            ssoSession = ssoSess;
 
-            PutUintBE((uint)(ssoHeader.Length + 4));
-            PutPacket(ssoHeader);
-            PutUintBE((uint)(ssoWupBuffer.Length + 4));
-            PutBytes(ssoWupBuffer.GetBytes());
-        }
-
-        public SsoMessage(byte[] data, byte[] cryptKey)
-            : base(data, TeaCryptor.Instance, cryptKey)
-        {
-            ssoHeader = new Header(GetBytes());
-            ssoWupBuffer = new Packet(ssoHeader.TakeAllBytes(out byte[] _));
-        }
-
-        public SsoMessage(byte[] data)
-            : base(data)
-        {
-            ssoHeader = new Header(GetBytes());
-            ssoWupBuffer = new Packet(ssoHeader.TakeAllBytes(out byte[] _));
-        }
-
-        public class Header : Packet
-        {
-            private readonly byte[] token = { };
-            private readonly byte[] unknownBytes0 = { };
-            private readonly byte[] unknownBytes1 = { };
-            private readonly string unknownString = $"||A{AppInfo.apkVersionName}.{AppInfo.appRevision}";
-
-            public readonly uint ssoSequence;
-            public readonly uint ssoSession;
-            public readonly string ssoCommand;
-
-            public Header(uint sequence, uint session, string command,
-                byte[] tgtToken)
-                : base()
+            ssoPktType = type;
+            ssoPayload = new ByteBuffer();
             {
-                ssoSequence = sequence;
-                ssoSession = session;
-                ssoCommand = command;
+                writer(ssoPayload);
+            }
+        }
 
-                if (tgtToken != null)
+        public SsoMessage(byte[] data, RequestPktType type)
+            : base()
+        {
+            ssoPktType = type;
+            var r = new ByteBuffer(data);
+            {
+                r.TakeUintBE(out var length);
                 {
-                    token = tgtToken;
+                    if (length > r.Length)
+                        throw new Exception("Invalid sso message.");
                 }
 
-                PutUintBE(ssoSequence);
-                PutUintBE(AppInfo.subAppId);
-                PutUintBE(AppInfo.subAppId);
-                PutHexString("01 00 00 00 00 00 00 00 00 00 01 00");
+                r.TakeUintBE(out ssoSequence);
 
-                PutUintBE((uint)(token.Length + 4));
-                PutBytes(token);
+                r.TakeUintBE(out var zeroUint);
+                {
+                    if (zeroUint != 0)
+                        throw new Exception("Invalid sso message.");
+                }
 
-                PutUintBE((uint)(ssoCommand.Length + 4));
-                PutString(ssoCommand);
+                r.TakeBytes(out var unknownBytes,
+                    ByteBuffer.Prefix.Uint32 | ByteBuffer.Prefix.WithPrefix);
 
-                PutUintBE((uint)(4 + 4));
-                PutUintBE((uint)ssoSession);
+                r.TakeString(out ssoCommand,
+                    ByteBuffer.Prefix.Uint32 | ByteBuffer.Prefix.WithPrefix);
 
-                PutUintBE((uint)(DeviceInfo.System.Imei.Length + 4));
-                PutString(DeviceInfo.System.Imei);
+                r.TakeBytes(out var session,
+                    ByteBuffer.Prefix.Uint32 | ByteBuffer.Prefix.WithPrefix);
+                {
+                    if (session.Length != 4)
+                        throw new Exception("Invalid sso message.");
 
-                PutUintBE((uint)(unknownBytes0.Length + 4));
-                PutBytes(unknownBytes0);
+                    ssoSession = ByteConverter.BytesToUInt32(session, 0);
+                }
 
-                PutUshortBE((ushort)(unknownString.Length + 2));
-                PutString(unknownString);
+                r.TakeUintBE(out zeroUint);
+                {
+                    if (zeroUint != 0)
+                        throw new Exception("Invalid sso message.");
+                }
 
-                PutUintBE((uint)(unknownBytes1.Length + 4));
-                PutBytes(unknownBytes1);
-            }
-
-            public Header(byte[] data) : base(data)
-            {
-                EatBytes(4);
-                TakeUintBE(out ssoSequence);
-
-                EatBytes(4);
-
-                TakeBytes(out token, Prefix.Uint32 | Prefix.WithPrefix);
-                TakeString(out ssoCommand, Prefix.Uint32 | Prefix.WithPrefix);
-
-                EatBytes(4);
-                TakeUintBE(out ssoSession);
-
-                EatBytes(4);
+                r.TakeAllBytes(out var bytes);
+                {
+                    ssoPayload = new ByteBuffer(bytes);
+                }
             }
         }
+
+        public RequestPktType GetPacketType() =>
+            ssoPktType;
+
+        public byte[] GetPayload() =>
+            ssoPayload.GetBytes();
+
+        public uint GetSequence() =>
+            ssoSequence;
+
+        public uint GetSession() =>
+            ssoSession;
+
+        public string GetCommand() =>
+            ssoCommand;
     }
 }
