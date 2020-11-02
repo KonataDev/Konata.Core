4ab72cc67d01e7e8fa0802e645b94a5e8d37319f
TheSnowfield <thesnowfield@moezsoft.com>
2020-11-03 03:33:01 +0800

    add new test

---
+++ Konata.Test/Tests/TestByteBuffer.cs
@@ -0,0 +1,21 @@
+﻿using Konata.Library.IO;
+using System;
+
+namespace Konata.Test.Tests
+{
+    class TestByteBuffer : Test
+    {
+        public override bool Run()
+        {
+            var buffer = new ByteBuffer();
+            {
+                buffer.PutBytes(new byte[0],
+                    ByteBuffer.Prefix.Uint32 | ByteBuffer.Prefix.WithPrefix);
+            }
+            buffer.TakeUintBE(out var length);
+
+            Assert(length == 4);
+            return true;
+        }
+    }
+}
