cfa085384866f6132de8eb3c7c18fb9e55b665b6
TheSnowfield <thesnowfield@moezsoft.com>
2020-11-03 05:27:06 +0800

    Fix barrier issue

--- Konata.Library.IO/ByteBuffer.cs
+++ Konata.Library.IO/ByteBuffer.cs
@@ -10,12 +10,12 @@ namespace Konata.Library.IO
         public enum Prefix
         {
             None = 0, // 前綴類型
-            Uint8 = 1,
-            Uint16 = 2,
-            Uint32 = 4,
+            Uint8 = 0b0001,
+            Uint16 = 0b0010,
+            Uint32 = 0b0100,
 
-            LengthOnly = 64, // 只包括數據長度
-            WithPrefix = 128,  // 包括數據長度和前綴長度
+            LengthOnly = 0, // 只包括數據長度
+            WithPrefix = 0b1000,  // 包括數據長度和前綴長度
         }
 
         private static readonly IOException eobException =
@@ -25,8 +25,8 @@ namespace Konata.Library.IO
 
         protected byte[] buffer;
         protected uint bufferLength;
-        private uint readPosition;
-        private uint writePosition;
+        protected uint readPosition;
+        protected uint writePosition;
 
         public ByteBuffer(byte[] data = null)
         {
@@ -343,29 +343,40 @@ namespace Konata.Library.IO
 
         public void PutBytes(byte[] value, Prefix prefixFlag = Prefix.None, byte limitedLength = 0)
         {
-            prefixFlag &= (Prefix)7;
+            int prefixLength = (int)prefixFlag & 0b0111;
             bool limited = limitedLength > 0; // 是否限制长度
             byte[] array; // 处理后的数据
             if (limited) // 限制长度时，写入数据长度=前缀+限制
             {
                 limitedLength = (byte)value.Length;
-                array = new byte[(uint)prefixFlag + limitedLength];
+                array = new byte[prefixLength + limitedLength];
                 int len = value.Length > limitedLength ? limitedLength : value.Length;
-                Buffer.BlockCopy(value, 0, array, (int)prefixFlag, len);
+                if (len > 0)
+                {
+                    Buffer.BlockCopy(value, 0, array, prefixLength, len);
+                }
             }
-            else if (prefixFlag > Prefix.None) // 不限制长度且有前缀时，写入数据长度=前缀+value长度
+            else if (prefixLength > 0) // 不限制长度且有前缀时，写入数据长度=前缀+value长度
             {
-                array = new byte[(uint)prefixFlag + value.Length];
-                Buffer.BlockCopy(value, 0, array, (int)prefixFlag, value.Length);
+                array = new byte[prefixLength + value.Length];
+                if (value.Length > 0)
+                {
+                    Buffer.BlockCopy(value, 0, array, prefixLength, value.Length);
+                }
             }
             else // 不限制又没有前缀，写入的就是value本身，不用处理，直接写入
             {
                 WriteData(value);
                 return;
             }
-            if (prefixFlag > Prefix.None) // 添加前缀，使用大端序
+            if (prefixLength > 0) // 添加前缀，使用大端序
             {
-                if (!InsertPrefix(array, 0, (uint)value.Length, prefixFlag))
+                int len = value.Length;
+                if ((prefixFlag & Prefix.WithPrefix) > 0)
+                {
+                    len += prefixLength;
+                }
+                if (!InsertPrefix(array, 0, (uint)len, (Prefix)prefixLength))
                 {
                     throw new IOException("Given prefix length is too small for value bytes."); // 给定的prefix不够填充value.Length，终止写入
                 }
