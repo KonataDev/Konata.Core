5cc2c8182134235d12ca37bd847e574d5b553241
TheSnowfield <thesnowfield@moezsoft.com>
2020-11-04 21:29:55 +0800

    Update test

--- Konata.Test/Tests/TestJceStructSimpleList.cs
+++ Konata.Test/Tests/TestJceStructSimpleList.cs
@@ -1,5 +1,6 @@
-﻿using Konata.Library.JceStruct;
-using System;
+﻿using System;
+using System.Linq;
+using Konata.Library.JceStruct;
 
 namespace Konata.Test.Tests
 {
@@ -7,15 +8,19 @@ namespace Konata.Test.Tests
     {
         public override bool Run()
         {
+            var bytes = new byte[]
+                { 0x00, 0x01, 0x02, 0x03 };
+
             var root = new Jce.Struct
             {
-                [0] = new Jce.SimpleList(new byte[]
-                {
-                    0x00, 0x01, 0x02, 0x03
-                })
+                [0] = new Jce.SimpleList(bytes)
             };
 
-            Print(Jce.Serialize(root));
+            var data = Jce.Serialize(root);
+            Print(data);
+
+            var deroot = Jce.Deserialize(data);
+            Assert(Enumerable.SequenceEqual(((Jce.SimpleList)deroot["0"]).Value, bytes));
 
             return true;
         }
