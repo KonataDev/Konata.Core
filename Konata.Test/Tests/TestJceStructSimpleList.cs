426d32278d1a33559cb1e551f71b55fd497d6791
TheSnowfield <thesnowfield@moezsoft.com>
2020-11-04 21:22:29 +0800

    Add new test for jce simple list

---
+++ Konata.Test/Tests/TestJceStructSimpleList.cs
@@ -0,0 +1,23 @@
+﻿using Konata.Library.JceStruct;
+using System;
+
+namespace Konata.Test.Tests
+{
+    class TestJceStructSimpleList : Test
+    {
+        public override bool Run()
+        {
+            var root = new Jce.Struct
+            {
+                [0] = new Jce.SimpleList(new byte[]
+                {
+                    0x00, 0x01, 0x02, 0x03
+                })
+            };
+
+            Print(Jce.Serialize(root));
+
+            return true;
+        }
+    }
+}
