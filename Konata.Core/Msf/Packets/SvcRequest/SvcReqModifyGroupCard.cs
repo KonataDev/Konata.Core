f83b9d71c491e483f3fda1b0ffdac84dc0364c98
TheSnowfield <thesnowfield@moezsoft.com>
2020-11-05 21:05:26 +0800

    Add SvcReqModifyGroupCard

---
+++ Konata.Core/Msf/Packets/SvcRequest/SvcReqModifyGroupCard.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Konata.Msf.Packets.Wup;
+using Konata.Library.JceStruct;
+
+namespace Konata.Msf.Packets.SvcRequest
+{
+    public class SvcReqModifyGroupCard : UniPacket
+    {
+        public SvcReqModifyGroupCard(uint groupUin, uint memberUin, string memberCard)
+
+            : base(0x03, "mqq.IMService.FriendListServiceServantObj",
+                  "ModifyGroupCardReq", "MGCREQ", 0x00, 0x00, 1761882478,
+                  (out Jce.Struct w) => w = new Jce.Struct
+                  {
+                      [0] = (Jce.Number)0,
+                      [1] = (Jce.Number)groupUin,
+                      [2] = (Jce.Number)0,
+
+                      [3] = (Jce.List)new Jce.List
+                      {
+                          [0] = (Jce.Struct)new Jce.Struct
+                          {
+                              [0] = (Jce.Number)memberUin,
+                              [1] = (Jce.Number)1,
+                              [2] = (Jce.String)memberCard,
+                              [3] = (Jce.Number)255,
+                              [4] = (Jce.String)"",
+                              [5] = (Jce.String)"",
+                              [6] = (Jce.String)"",
+                          }
+                      }
+                  })
+        {
+        }
+    }
+}
