22acb7fc25c1b73a83ae2cb8a5dd88a02707cdf8
TheSnowfield <thesnowfield@moezsoft.com>
2020-11-05 14:27:29 +0800

    Add SvcRspRegister

---
+++ Konata.Core/Msf/Packets/SvcResponse/SvcRspRegister.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Konata.Msf.Packets.Wup;
+using Konata.Library.JceStruct;
+
+namespace Konata.Msf.Packets.SvcResponse
+{
+    public class SvcRspRegister : UniPacket
+    {
+        public long uin;
+        public long bid;
+        public string ipAddress;
+        public bool status;
+
+        public SvcRspRegister(byte[] response)
+            : base(response, (object userdata, Jce.Struct r) =>
+            {
+                var p = (SvcRspRegister)userdata;
+                {
+                    p.uin = (Jce.Number)r["0.0"];
+                    p.bid = (Jce.Number)r["0.1"];
+                    p.status = (Jce.Number)r["0.9"] == 1;
+                    p.ipAddress = (string)(Jce.String)r["0.10"];
+                }
+            })
+        {
+
+        }
+    }
+}
