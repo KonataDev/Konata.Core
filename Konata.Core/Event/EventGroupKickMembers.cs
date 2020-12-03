16178e9a31616318ae610fc438711ac9a915712c
TheSnowfield <thesnowfield@moezsoft.com>
2020-12-04 07:38:13 +0800

    Rewrite Oidb 0x8a0_0

---
+++ Konata.Core/Event/EventGroupKickMembers.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+using Konata.Runtime.Base.Event;
+
+namespace Konata.Core.Event
+{
+    public class EventGroupKickMembers : KonataEventArgs
+    {
+        public uint GroupUin { get; set; }
+
+        public uint[] MembersUin { get; set; }
+
+        /// <summary>
+        /// <b>KickMembers</b>: Block the members <br/>
+        /// </summary>
+        public bool ToggleType { get; set; }
+    }
+}
--- Konata.Core/Service/SSO/OidbSvc/Oidb0x8a0_0.cs
+++ Konata.Core/Service/SSO/OidbSvc/Oidb0x8a0_0.cs
@@ -1,13 +1,15 @@
 ﻿using System;
 using System.Text;
 
-using Konata.Core.Packet;
 using Konata.Core.Event;
+using Konata.Core.Packet;
+using Konata.Core.Manager;
+using Konata.Core.Packet.Oidb.OidbModel;
 using Konata.Runtime.Base.Event;
 
 namespace Konata.Core.Service.OidbSvc
 {
-    [SSOService("OidbSvc.0x8a0_0", "Kick members in the group")]
+    [SSOService("OidbSvc.0x8a0_0", "Kick members in the group by batch")]
     class Oidb0x8a0_0 : ISSOService
     {
         public bool HandleInComing(EventSsoFrame ssoMessage, out KonataEventArgs output)
@@ -15,9 +17,28 @@ namespace Konata.Core.Service.OidbSvc
             throw new NotImplementedException();
         }
 
-        public bool HandleOutGoing(KonataEventArgs original, out byte[] message)
+        public bool HandleOutGoing(KonataEventArgs eventArg, out byte[] output)
         {
-            throw new NotImplementedException();
+            output = null;
+
+            if (eventArg is EventGroupKickMembers e)
+            {
+                var sigManager = e.Owner.GetComponent<UserSigManager>();
+                var ssoManager = e.Owner.GetComponent<SsoInfoManager>();
+                var oidbRequest = new OidbCmd0x8a0_0(e.GroupUin, e.MembersUin, e.ToggleType);
+
+                if (EventSsoFrame.Create("OidbSvc.0x8a0_0", PacketType.TypeB,
+                    ssoManager.NewSequence, ssoManager.Session, oidbRequest, out var ssoFrame))
+                {
+                    if (EventServiceMessage.Create(ssoFrame, AuthFlag.D2Authentication,
+                        sigManager.Uin, sigManager.D2Token, sigManager.D2Key, out var toService))
+                    {
+                        return EventServiceMessage.Build(toService, out output);
+                    }
+                }
+            }
+
+            return false;
         }
     }
 }
