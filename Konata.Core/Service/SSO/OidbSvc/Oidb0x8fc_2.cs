c768cc49d73799978f9bcf5bb676748220783e72
TheSnowfield <thesnowfield@moezsoft.com>
2020-12-04 07:44:21 +0800

    Rewrite Oidb 0x8fc_2

--- Konata.Core/Service/SSO/OidbSvc/Oidb0x8fc_2.cs
+++ Konata.Core/Service/SSO/OidbSvc/Oidb0x8fc_2.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Text;
 
-using Konata.Core.Packet;
 using Konata.Core.Event;
+using Konata.Core.Packet;
+using Konata.Core.Manager;
+using Konata.Core.Packet.Oidb.OidbModel;
 using Konata.Runtime.Base.Event;
 
 namespace Konata.Core.Service.OidbSvc
@@ -15,9 +17,29 @@ namespace Konata.Core.Service.OidbSvc
             throw new NotImplementedException();
         }
 
-        public bool HandleOutGoing(KonataEventArgs original, out byte[] message)
+        public bool HandleOutGoing(KonataEventArgs eventArg, out byte[] output)
         {
-            throw new NotImplementedException();
+            output = null;
+
+            if (eventArg is EventGroupSetSpecialTitle e)
+            {
+                var sigManager = e.Owner.GetComponent<UserSigManager>();
+                var ssoManager = e.Owner.GetComponent<SsoInfoManager>();
+                var oidbRequest = new OidbCmd0x8fc_2(e.GroupUin, e.MemberUin, e.SpecialTitle,
+                    e.TimeSeconds ?? uint.MaxValue);
+
+                if (EventSsoFrame.Create("OidbSvc.0x8fc_2", PacketType.TypeB,
+                    ssoManager.NewSequence, ssoManager.Session, oidbRequest, out var ssoFrame))
+                {
+                    if (EventServiceMessage.Create(ssoFrame, AuthFlag.D2Authentication,
+                        sigManager.Uin, sigManager.D2Token, sigManager.D2Key, out var toService))
+                    {
+                        return EventServiceMessage.Build(toService, out output);
+                    }
+                }
+            }
+
+            return false;
         }
     }
 }
