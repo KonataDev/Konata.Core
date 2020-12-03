664be45162b95f12411511daa10b03213e3fa111
TheSnowfield <thesnowfield@moezsoft.com>
2020-12-04 07:31:10 +0800

    Rewrite Oidb 0x570_8

---
+++ Konata.Core/Event/EventGroupMuteMember.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+using Konata.Runtime.Base.Event;
+
+namespace Konata.Core.Event
+{
+    public class EventGroupMuteMember : KonataEventArgs
+    {
+        public uint GroupUin { get; set; }
+
+        public uint MemberUin { get; set; }
+
+        /// <summary>
+        /// <b>MuteMember</b>: Mute time <br/>
+        /// </summary>
+        public uint? TimeSeconds { get; set; }
+    }
+}
