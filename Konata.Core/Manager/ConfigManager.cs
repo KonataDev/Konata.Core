fa92696e029073562cf2c84c51b2ab47b4a8f3b2
TheSnowfield <thesnowfield@moezsoft.com>
2020-12-04 08:28:06 +0800

    Add ConfigManager

---
+++ Konata.Core/Manager/ConfigManager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Konata.Runtime.Base;
+
+namespace Konata.Core.Manager
+{
+    public class ConfigManager : Component
+    {
+
+    }
+}
