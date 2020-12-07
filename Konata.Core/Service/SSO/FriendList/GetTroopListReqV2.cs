6cae5ac1ff7b8d17056ca7a16b339db737af18ac
TheSnowfield <thesnowfield@moezsoft.com>
2020-12-07 08:47:42 +0800

    Rewrite friendlist.GetTroopListReqV2

--- Konata.Core/Service/SSO/FriendList/GetTroopListReqV2.cs
+++ Konata.Core/Service/SSO/FriendList/GetTroopListReqV2.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Text;
+
 using Konata.Core.Event;
 using Konata.Core.Packet;
+using Konata.Core.Manager;
+using Konata.Core.Packet.SvcRequest;
 using Konata.Runtime.Base.Event;
 
 namespace Konata.Core.Service.Friendlist
@@ -9,14 +12,31 @@ namespace Konata.Core.Service.Friendlist
     [SSOService("friendlist.GetTroopListReqV2", "Pull group list")]
     public class GetTroopListReqV2 : ISSOService
     {
-        public bool HandleInComing(EventSsoFrame ssoMessage, out KonataEventArgs output)
-        {
-            throw new NotImplementedException();
-        }
+        public bool HandleInComing(EventSsoFrame ssoFrame, out KonataEventArgs output)
+           => throw new NotImplementedException();
 
-        public bool HandleOutGoing(KonataEventArgs original, out byte[] message)
+        public bool HandleOutGoing(KonataEventArgs eventArg, out byte[] output)
         {
-            throw new NotImplementedException();
+            output = null;
+
+            if (eventArg is EventAccountPullTroopList e)
+            {
+                var sigManager = e.Owner.GetComponent<UserSigManager>();
+                var ssoManager = e.Owner.GetComponent<SsoInfoManager>();
+                var svcRequest = new SvcReqGetTroopListReqV2Simplify(e.SelfUin);
+
+                if (EventSsoFrame.Create("friendlist.ModifyGroupCardReq", PacketType.TypeB,
+                    ssoManager.NewSequence, ssoManager.Session, svcRequest, out var ssoFrame))
+                {
+                    if (EventServiceMessage.Create(ssoFrame, AuthFlag.D2Authentication,
+                        sigManager.Uin, sigManager.D2Token, sigManager.D2Key, out var toService))
+                    {
+                        return EventServiceMessage.Build(toService, out output);
+                    }
+                }
+            }
+
+            return false;
         }
     }
 }
