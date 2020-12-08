afe8fb02f962d11239cd799cce1a67327058ba43
TheSnowfield <thesnowfield@moezsoft.com>
2020-12-08 13:04:32 +0800

    Add some methods to Bot

--- Konata.Core/Bot.cs
+++ Konata.Core/Bot.cs
@@ -1,17 +1,108 @@
 ﻿using System;
 
+using Konata.Core.Event;
 using Konata.Core.Manager;
+using Konata.Core.Service;
 using Konata.Runtime.Base;
 
 namespace Konata.Core
 {
     public class Bot : Entity
     {
-        public Bot(uint botUin, string botPassword)
+        public static async void Login(Bot bot)
         {
-            AddComponent<ConfigManager>();
-            AddComponent<SsoInfoManager>();
-            AddComponent<UserSigManager>().InitializeProfile(botUin, botPassword);
+            var e = new EventWtLogin
+            {
+                Owner = bot,
+                EventType = EventWtLogin.Type.Tgtgt,
+            };
         }
+
+        /// <summary>
+        /// Submit Slider ticket
+        /// </summary>
+        /// <param name="bot"><b>[In]</b> The bot instance. </param>
+        /// <param name="ticket"><b>[In]</b> Slider ticket</param>
+        public static async void SubmitSliderTicket(Bot bot, string ticket)
+        {
+            var e = new EventWtLogin
+            {
+                Owner = bot,
+                WtLoginCaptchaResult = ticket,
+                EventType = EventWtLogin.Type.CheckSlider,
+            };
+        }
+
+        /// <summary>
+        /// Submit SMS code.
+        /// </summary>
+        /// <param name="bot"><b>[In]</b> The bot instance. </param>
+        /// <param name="code"><b>[In]</b> SMS code</param>
+        public static async void SubmitSMSCode(Bot bot, string code)
+        {
+            var e = new EventWtLogin
+            {
+                Owner = bot,
+                WtLoginCaptchaResult = code,
+                EventType = EventWtLogin.Type.CheckSMS,
+            };
+        }
+
+        /// <summary>
+        /// Kick a member in the specific group.
+        /// </summary>
+        /// <param name="bot"><b>[In]</b> The bot instance. </param>
+        /// <param name="groupUin"><b>[In]</b> Group uin being operated. </param>
+        /// <param name="memberUin"><b>[In]</b> Member uin being operated. </param>
+        /// <param name="preventRequest"><b>[In]</b> Flag to prevent member request or no. </param>
+        public static async void GroupKickMember(Bot bot, uint groupUin,
+            uint memberUin, bool preventRequest)
+        {
+            var e = new EventGroupKickMember
+            {
+                Owner = bot,
+                GroupUin = groupUin,
+                MemberUin = memberUin,
+                ToggleType = preventRequest
+            };
+        }
+
+        /// <summary>
+        /// Promote a member to admin in the specific group.
+        /// </summary>
+        /// <param name="bot"><b>[In]</b> The bot instance. </param>
+        /// <param name="groupUin"><b>[In]</b> Group uin being operated. </param>
+        /// <param name="memberUin"><b>[In]</b> Member uin being operated. </param>
+        /// <param name="toggleAdmin"><b>[In]</b> Flag to toggle set or unset. </param>
+        public static async void GroupPromoteAdmin(Bot bot, uint groupUin,
+            uint memberUin, bool toggleAdmin)
+        {
+            var e = new EventGroupPromoteAdmin
+            {
+                Owner = bot,
+                GroupUin = groupUin,
+                MemberUin = memberUin,
+                ToggleType = toggleAdmin
+            };
+        }
+
+    }
+
+    public static class BotFather
+    {
+        public static Bot Build(uint botUin, string botPassword)
+        => LoadAllComponents(new Bot(), botUin, botPassword);
+
+        private static Bot LoadAllComponents(Bot bot, uint botUin, string botPassword)
+        {
+            bot.AddComponent<EventComponent>();
+            bot.AddComponent<ConfigManager>();
+            bot.AddComponent<SsoInfoManager>();
+            bot.AddComponent<UserSigManager>().InitializeProfile(botUin, botPassword);
+
+            return bot;
+        }
+
+        private
     }
 }
