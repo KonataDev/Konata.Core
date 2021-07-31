0f20c941684f714f9af9850e1c20634a82691c88
TheSnowfield <thesnowfield@moezsoft.com>
2021-07-31 20:37:57 +0800

    Business attribute

---
+++ Attributes/BusinessLogicAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Konata.Core.Attributes
+{
+    /// <summary>
+    /// Business logic Attribute
+    /// </summary>
+    internal class BusinessLogicAttribute : Attribute
+    {
+        public string LogicName { get; set; }
+
+        public string Description { get; set; }
+
+        public BusinessLogicAttribute(string name, string description)
+        {
+            LogicName = name;
+            Description = description;
+        }
+    }
+}
