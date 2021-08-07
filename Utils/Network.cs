31941d47ddc1f902f62388dfc25881e7bd0ce1ec
TheSnowfield <thesnowfield@moezsoft.com>
2021-08-08 06:47:45 +0800

    File download

--- Utils/Network.cs
+++ Utils/Network.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.IO;
+using System.Net;
 using System.Net.NetworkInformation;
+using System.Threading.Tasks;
 
 namespace Konata.Utils
 {
@@ -26,5 +29,34 @@ namespace Konata.Utils
 
             return long.MaxValue;
         }
+
+        /// <summary>
+        /// Download file
+        /// </summary>
+        /// <param name="url"></param>
+        /// <param name="timeout"></param>
+        /// <returns></returns>
+        public static async Task<byte[]> Download(string url, int timeout = 8000)
+        {
+            // Create request
+            var request = WebRequest.CreateHttp(url);
+            {
+                request.Timeout = timeout;
+                request.ReadWriteTimeout = timeout;
+            }
+
+            var response = await request.GetResponseAsync();
+            {
+                using (var stream = new MemoryStream())
+                {
+                    response.GetResponseStream().CopyTo(stream);
+                    return stream.ToArray();
+                }
+            }
+        }
+
+        public static string UintToIPBE(uint ip)
+            => $"{(ip >> 0) & 0xFF}." + $"{(ip >> 8) & 0xFF}." +
+                $"{(ip >> 16) & 0xFF}." + $"{(ip >> 24) & 0xFF}";
     }
 }
