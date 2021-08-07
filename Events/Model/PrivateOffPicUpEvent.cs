313de4a5136ef29c053679631e28eb8bacf520d4
TheSnowfield <thesnowfield@moezsoft.com>
2021-08-08 06:57:46 +0800

    Upgrade to dotnet standard 2.1

---
+++ Events/Model/PrivateOffPicUpEvent.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Konata.Core.Message.Model;
+
+namespace Konata.Core.Events.Model
+{
+    public class PrivateOffPicUpEvent : ProtocolEvent
+    {
+        public List<ImageChain> Images { get; set; }
+    }
+}
