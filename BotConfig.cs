67f937e3b6d72ad14f42c911a13ae61f8ddb1357
TheSnowfield <thesnowfield@moezsoft.com>
2021-08-13 13:11:40 +0800

    Default config

---
+++ BotConfig.cs
@@ -0,0 +1,42 @@
+﻿// ReSharper disable MemberCanBePrivate.Global
+// ReSharper disable UnusedAutoPropertyAccessor.Global
+
+namespace Konata.Core
+{
+    public class BotConfig
+    {
+        public bool UseIPv6Connection { get; private set; }
+
+        public bool ReConnectWhileLinkDown { get; private set; }
+
+        public uint ReConnectTryCount { get; private set; }
+
+        #region Custom Server IP
+
+        public string CustomHost { get; private set; }
+
+        #endregion
+
+        #region Image upload
+
+        public int ImageChunkSize { get; private set; }
+
+        #endregion
+
+        /// <summary>
+        /// Get a default config
+        /// </summary>
+        /// <returns></returns>
+        public static BotConfig Default()
+        {
+            return new BotConfig
+            {
+                UseIPv6Connection = false,
+                ReConnectWhileLinkDown = true,
+                ReConnectTryCount = 3,
+                CustomHost = null,
+                ImageChunkSize = 4096,
+            };
+        }
+    }
+}
