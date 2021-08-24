695cac00a859f4a9ece35af786f16a3ab4c9c7c3
TheSnowfield <thesnowfield@moezsoft.com>
2021-08-24 20:41:35 +0800

    Audio detection

--- Message/Model/RecordChain.cs
+++ Message/Model/RecordChain.cs
@@ -1,4 +1,14 @@
-﻿// ReSharper disable UnusedAutoPropertyAccessor.Global
+﻿using System;
+using System.Linq;
+using Konata.AvCodec;
+using Konata.Core.Utils.Crypto;
+using Konata.Core.Utils.IO;
+using Konata.Core.Utils.FileFormat;
+
+// ReSharper disable RedundantAssignment
+// ReSharper disable RedundantCaseLabel
+// ReSharper disable NotAccessedVariable
+// ReSharper disable UnusedAutoPropertyAccessor.Global
 // ReSharper disable MemberCanBePrivate.Global
 
 namespace Konata.Core.Message.Model
@@ -11,12 +21,35 @@ namespace Konata.Core.Message.Model
 
         public string FileHash { get; }
 
+        public byte[] HashData { get; }
+
+        public byte[] FileData { get; }
+
+        public uint FileLength { get; }
+
+        public uint TimeSeconds { get; }
+
+        public RecordType RecordType { get; }
+
         private RecordChain(string url, string fileName, string fileHash)
             : base(ChainType.Record, ChainMode.Singleton)
         {
             RecordUrl = url;
             FileName = fileName;
             FileHash = fileHash;
+        }
+
+        private RecordChain(byte[] data, uint timesec,
+            byte[] md5, string md5str, RecordType type)
+            : base(ChainType.Record, ChainMode.Singleton)
+        {
+            FileData = data;
+            FileLength = (uint) data.Length;
+            TimeSeconds = timesec;
+            HashData = md5;
+            FileHash = md5str;
+            RecordType = type;
+            FileName = md5str;
         }
 
         /// <summary>
@@ -33,6 +66,114 @@ namespace Konata.Core.Message.Model
         }
 
         /// <summary>
+        /// Create an audio chain
+        /// </summary>
+        /// <param name="audio"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public static RecordChain Create(byte[] audio)
+        {
+            // Detect image type
+            if (FileFormat.DetectAudio(audio, out var type,
+                out var timeSec, out var sampleRate, out var channel))
+            {
+                byte[] audioData = null;
+                RecordType audioType = RecordType.SILK;
+
+                // Process
+                switch (type)
+                {
+                    // Amr format
+                    // We no need to convert it
+                    case FileFormat.AudioFormat.AMR:
+                        audioData = audio;
+                        audioType = RecordType.AMR;
+                        break;
+
+                    // Silk v3 for tx use
+                    case FileFormat.AudioFormat.TENSILKV3:
+                        audioData = audio;
+                        audioType = RecordType.SILK;
+                        break;
+
+                    // Normal silk v3
+                    // We need to append a header 0x02
+                    // and remove 0xFFFF end for it
+                    case FileFormat.AudioFormat.SILKV3:
+                    {
+                        audioType = RecordType.SILK;
+                        audioData = new byte[] {0x02}
+                            .Concat(audio[..^2]).ToArray();
+                        break;
+                    }
+
+                    // Need to convert
+                    default:
+                    case FileFormat.AudioFormat.MP3:
+                    case FileFormat.AudioFormat.WAV:
+                    case FileFormat.AudioFormat.UNKNOWN:
+                    {
+                        var pcmData = Array.Empty<byte>();
+                        var silkData = Array.Empty<byte>();
+
+                        // TODO:
+                        // Convert audio format to silk
+
+                        switch (type)
+                        {
+                            // Decode Mp3 to pcm
+                            case FileFormat.AudioFormat.MP3:
+                                throw new NotImplementedException();
+
+                            // Decode Wav to pcm
+                            case FileFormat.AudioFormat.WAV:
+                                throw new NotImplementedException();
+
+                            // Cannot convert unknown type
+                            case FileFormat.AudioFormat.UNKNOWN:
+                                return null;
+                        }
+
+                        // TODO:
+                        // Resample the pcm to
+                        // signed 16bit mono 24kHz format 
+
+                        // Convert pcm data to silkv3
+                        if (pcmData.Length != 0 && SilkCodec
+                            .Encode(pcmData, 24000, out silkData).Result)
+                        {
+                            // Set audio information
+                            audioData = silkData;
+                            audioType = RecordType.SILK;
+                        }
+
+                        break;
+                    }
+                }
+
+                // Audio MD5
+                var audioMD5 = new Md5Cryptor().Encrypt(audioData);
+                var audioMD5Str = ByteConverter.Hex(audioMD5).ToUpper();
+
+                return new RecordChain(audioData, (uint) timeSec,
+                    audioMD5, audioMD5Str, audioType);
+            }
+
+            return null;
+        }
+
+        /// <summary>
+        /// Create an record chain from plain base64 <br />
+        /// Not incuding the header 'base64://'
+        /// </summary>
+        /// <param name="base64"></param>
+        /// <returns></returns>
+        public static RecordChain CreateFromBase64(string base64)
+        {
+            return Create(ByteConverter.UnBase64(base64));
+        }
+
+        /// <summary>
         /// Parse the code
         /// </summary>
         /// <param name="code"></param>
@@ -45,5 +186,11 @@ namespace Konata.Core.Message.Model
         public override string ToString()
             => $"[KQ:record," +
                $"file={FileName}]";
+    }
+
+    public enum RecordType
+    {
+        AMR = 1005,
+        SILK = 1002,
     }
 }
---
+++ Utils/FileFormat/DetectAudio.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using Konata.Core.Utils.IO;
+
+namespace Konata.Core.Utils.FileFormat
+{
+    public static partial class FileFormat
+    {
+        public enum AudioFormat
+        {
+            UNKNOWN,
+            WAV,
+            MP3,
+            SILKV3,
+            TENSILKV3,
+            AMR,
+        }
+
+        /// <summary>
+        /// Detect audio type
+        /// </summary>
+        /// <param name="data"></param>
+        /// <param name="type"></param>
+        /// <param name="timesec"></param>
+        /// <param name="samplerate"></param>
+        /// <param name="channel"></param>
+        /// <returns></returns>
+        public static bool DetectAudio(byte[] data, out AudioFormat type,
+            out int timesec, out int samplerate, out int channel)
+        {
+            var buffer = new ByteBuffer(data);
+            {
+                buffer.PeekUintBE(0, out var value);
+
+                // SILKV3
+                //  #  !  S  I  L  K
+                // +--+--+--+--+--+--+
+                // |23|21|53|49|4C|4B|
+                // +--+--+--+--+--+--+
+                // 0           4     6  
+                if (value == 0x23215349)
+                {
+                    type = AudioFormat.SILKV3;
+                    return DetectSilkV3(buffer, out timesec,
+                        out samplerate, out channel);
+                }
+
+                // TENSILKV3
+                //     #  !  S  I  L  K
+                // +--+--+--+--+--+--+--+
+                // |02|23|21|53|49|4C|4B|
+                // +--+--+--+--+--+--+--+
+                // 0           4        7  
+                if (value == 0x02232153)
+                {
+                    type = AudioFormat.TENSILKV3;
+                    return DetectSilkV3(buffer, out timesec,
+                        out samplerate, out channel);
+                }
+
+                // WAV
+                //  R  I  F  F  .  .  .  .  W  A  V  E  f  m  t
+                // +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+                // |52|49|46|46|00|00|00|00|57|41|56|45|66|6D|74|
+                // +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+                // 0           4           8           12       15
+                if (value == 0x52494646 &&
+                    buffer.PeekUintBE(8, out _) == 0x57415645)
+                {
+                    type = AudioFormat.WAV;
+                    return DetectWav(buffer, out timesec,
+                        out samplerate, out channel);
+                }
+
+                // MP3
+                //  I  D  3
+                // +--+--+--+
+                // |49|44|33|
+                // +--+--+--+
+                // 0        3
+                if (value >> 8 == 0x494433)
+                {
+                    type = AudioFormat.MP3;
+                    return DetectMp3(buffer, out timesec,
+                        out samplerate, out channel);
+                }
+
+                // TODO:
+                // AMR
+            }
+
+            type = AudioFormat.UNKNOWN;
+            timesec = samplerate = channel = 0;
+            return false;
+        }
+
+        private static bool DetectSilkV3(ByteBuffer buffer,
+            out int timesec, out int samplerate, out int channel)
+        {
+            // Fixed 1chn and 24kHz
+            channel = 1;
+            samplerate = 24000;
+
+            // Fake (?
+            timesec = (int) Math.Ceiling
+                (buffer.RemainLength / 2886.923);
+            return true;
+        }
+
+        private static bool DetectWav(ByteBuffer buffer,
+            out int timesec, out int samplerate, out int channel)
+        {
+            // |ChunkSize  |Fmt  |Chn  |SampleRate |ByteRate   |Align|Bits |d  a  t  a |ChunkSize
+            // +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+            // |10|00|00|00|01|00|02|00|22|56|00|00|88|58|01|00|04|00|10|00|64|61|74|61|00|00|00|00|
+            // +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+            // 16          20          24          28          32          36          40          43         
+
+            // Only supported PCM
+            timesec = samplerate = channel = 0;
+            if (buffer.PeekUintBE(20, out _) != 0x1000) return false;
+
+            var bps = buffer.PeekUintBE(34, out _);
+            var shunksize = buffer.PeekUintBE(40, out _);
+
+            channel = (int) buffer.PeekUshortLE(22, out _);
+            samplerate = (int) buffer.PeekUintLE(24, out _);
+            timesec = (int) Math.Ceiling((((double) shunksize / channel / ((int) (bps / 8))) / samplerate));
+            return true;
+        }
+
+        private static bool DetectMp3(ByteBuffer buffer,
+            out int timesec, out int samplerate, out int channel)
+        {
+            // TODO:
+            timesec = samplerate = channel = 0;
+            return false;
+        }
+    }
+}
