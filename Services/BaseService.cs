347641b785cff3dc20a81b3dd38930b94f7a328c
TheSnowfield <thesnowfield@moezsoft.com>
2021-09-12 03:00:23 +0800

    Stage files

--- Services/BaseService.cs
+++ Services/BaseService.cs
@@ -1,6 +1,8 @@
 ﻿using Konata.Core.Events;
 using Konata.Core.Packets;
 
+// ReSharper disable InvertIf
+
 namespace Konata.Core.Services
 {
     public abstract class BaseService<TEvent> : IService
@@ -13,8 +15,8 @@ namespace Konata.Core.Services
         /// <param name="keystore"></param>
         /// <param name="output"></param>
         /// <returns></returns>
-        public virtual bool Parse(SSOFrame input,
-            BotKeyStore keystore, out ProtocolEvent output)
+        protected virtual bool Parse(SSOFrame input, 
+            BotKeyStore keystore, out TEvent output)
         {
             output = null;
             return false;
@@ -48,9 +50,29 @@ namespace Konata.Core.Services
         /// <param name="newSequence"></param>
         /// <param name="output"></param>
         /// <returns></returns>
-        public bool Build(Sequence sequence, ProtocolEvent input, BotKeyStore
-            keystore, BotDevice device, out int newSequence, out byte[] output)
+        bool IService.Build(Sequence sequence, ProtocolEvent input,
+            BotKeyStore keystore, BotDevice device, out int newSequence, out byte[] output)
             => Build(sequence, (TEvent) input, keystore, device, out newSequence, out output);
+
+        /// <summary>
+        /// Parse packet
+        /// </summary>
+        /// <param name="input"></param>
+        /// <param name="keystore"></param>
+        /// <param name="output"></param>
+        /// <returns></returns>
+        bool IService.Parse(SSOFrame input, BotKeyStore keystore, out ProtocolEvent output)
+        {
+            output = null;
+            
+            if (Parse(input, keystore, out var dirtyCs))
+            {
+                output = dirtyCs;
+                return true;
+            }
+
+            return false;
+        }
 
         /// <summary>
         /// TODO: sso service refactor
