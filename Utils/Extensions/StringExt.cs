9869f110c80357994d341052b694c1f4b331fcb5
TheSnowfield <thesnowfield@moezsoft.com>
2022-03-01 04:39:24 +0800

    Some utils and extensions, adler32 and crc32 algorithms

---
+++ Utils/Extensions/StringExt.cs
@@ -0,0 +1,25 @@
+﻿using System.Runtime.CompilerServices;
+using Konata.Core.Utils.IO;
+
+namespace Konata.Core.Utils.Extensions;
+
+public static class StringExt
+{
+    /// <summary>
+    /// Convert hex string to bytes 
+    /// </summary>
+    /// <param name="data"></param>
+    /// <returns></returns>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static byte[] UnHex(this string data)
+        => ByteConverter.UnHex(data);
+
+    /// <summary>
+    /// Convert base64 string to bytes
+    /// </summary>
+    /// <param name="data"></param>
+    /// <returns></returns>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static byte[] Atob(this string data)
+        => ByteConverter.UnBase64(data);
+}
