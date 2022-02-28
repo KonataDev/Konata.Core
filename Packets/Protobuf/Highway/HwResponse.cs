677781c6b71a8a1fd7099d01a839ad613e5295ad
TheSnowfield <thesnowfield@moezsoft.com>
2022-03-01 04:38:17 +0800

    Refactor highway packets

--- Packets/Protobuf/Highway/HwResponse.cs
+++ Packets/Protobuf/Highway/HwResponse.cs
@@ -5,75 +5,74 @@ using Konata.Core.Utils.Protobuf;
 // ReSharper disable MemberCanBePrivate.Global
 // ReSharper disable UnusedAutoPropertyAccessor.Global
 
-namespace Konata.Core.Packets.Protobuf.Highway
+namespace Konata.Core.Packets.Protobuf.Highway;
+
+internal class HwResponse : ProtoTreeRoot
 {
-    public class HwResponse : ProtoTreeRoot
+    public byte Version { get; private set; }
+
+    public uint PeerUin { get; private set; }
+
+    public string Command { get; private set; }
+
+    public uint Sequence { get; private set; }
+
+    public uint RetryTimes { get; private set; }
+
+    public uint CommandId { get; private set; }
+
+    public uint LocaleId { get; private set; }
+
+    private HwResponse(byte[] pbhead)
+        : base(pbhead, true)
     {
-        public byte Version { get; private set; }
-
-        public uint PeerUin { get; private set; }
-
-        public string Command { get; private set; }
-
-        public uint Sequence { get; private set; }
-
-        public uint RetryTimes { get; private set; }
-
-        public uint CommandId { get; private set; }
-
-        public uint LocaleId { get; private set; }
-
-        private HwResponse(byte[] pbhead)
-            : base(pbhead, true)
+        GetTree("0A", r =>
         {
-            GetTree("0A", r =>
-            {
-                Version = (byte) r.GetLeafVar("08");
+            Version = (byte) r.GetLeafVar("08");
 
-                // Uin string
-                PeerUin = uint.Parse(r.GetLeafString("12"));
+            // Uin string
+            PeerUin = uint.Parse(r.GetLeafString("12"));
 
-                // Command
-                Command = r.GetLeafString("1A");
+            // Command
+            Command = r.GetLeafString("1A");
 
-                // Sequence
-                Sequence = (uint) r.GetLeafVar("20");
+            // Sequence
+            Sequence = (uint) r.GetLeafVar("20");
 
-                // Retry times
-                RetryTimes = (uint) r.GetLeafVar("28");
+            // Retry times
+            RetryTimes = (uint) r.GetLeafVar("28");
 
-                // Command id
-                CommandId = (uint) r.GetLeafVar("40");
+            // Command id
+            CommandId = (uint) r.GetLeafVar("40");
 
-                // Locale id
-                LocaleId = (uint) r.GetLeafVar("50");
-            });
-        }
+            // Locale id
+            LocaleId = (uint) r.GetLeafVar("50");
+        });
+    }
 
-        /// <summary>
-        /// Parse a response
-        /// </summary>
-        /// <param name="data"></param>
-        /// <returns></returns>
-        public static HwResponse Parse(byte[] data)
+    /// <summary>
+    /// Parse a response
+    /// </summary>
+    /// <param name="data"></param>
+    /// <returns></returns>
+    internal static HwResponse Parse(byte[] data)
+    {
+        var buffer = new ByteBuffer(data);
         {
-            var buffer = new ByteBuffer(data);
-            {
-                buffer.EatBytes(1);
+            buffer.EatBytes(1);
 
-                // PBhead length
-                buffer.TakeUintBE(out var pbheadLen);
+            // PBhead length
+            buffer.TakeUintBE(out var pbheadLen);
 
-                // Payload length
-                buffer.EatBytes(4);
+            // Payload length
+            buffer.EatBytes(4);
 
-                // PBhead data
-                buffer.TakeBytes(out var pbhead, pbheadLen);
+            // PBhead data
+            buffer.TakeBytes(out var pbhead, pbheadLen);
 
-                buffer.EatBytes(1);
+            buffer.EatBytes(1);
 
-                return new(pbhead);
-            }
+            return new(pbhead);
         }
     }
 }
