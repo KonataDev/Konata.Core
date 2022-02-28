02fe4461f6bbb1ec4b06f89003fc557d5759ff74
TheSnowfield <thesnowfield@moezsoft.com>
2022-03-01 04:36:49 +0800

    Plain text chain to text chain

---
+++ Message/Model/TextChain.cs
@@ -0,0 +1,23 @@
+﻿namespace Konata.Core.Message.Model;
+
+public class TextChain : BaseChain
+{
+    public string Content { get; }
+
+    private TextChain(string content)
+        : base(ChainType.Text, ChainMode.Multiple)
+    {
+        Content = content;
+    }
+
+    /// <summary>
+    /// Create a text chain
+    /// </summary>
+    /// <param name="text"></param>
+    /// <returns></returns>
+    public static TextChain Create(string text)
+        => new(text);
+
+    public override string ToString()
+        => Content;
+}
