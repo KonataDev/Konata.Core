daf21fc33ba3472a825911a511356caa96d133b9
TheSnowfield <thesnowfield@moezsoft.com>
2022-03-01 04:35:04 +0800

    Common server info

---
+++ Common/ServerInfo.cs
@@ -0,0 +1,23 @@
+﻿namespace Konata.Core.Common;
+
+public class ServerInfo
+{
+    /// <summary>
+    /// Server host
+    /// </summary>
+    public string Host { get; }
+
+    /// <summary>
+    /// Server port
+    /// </summary>
+    public ushort Port { get; }
+
+    public ServerInfo(string host, ushort port)
+    {
+        Host = host;
+        Port = port;
+    }
+
+    public override string ToString()
+        => $"{Host}:{Port}";
+}
