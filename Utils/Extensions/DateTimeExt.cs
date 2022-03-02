289c6451215de723fcbd57b655b9df2dddec4fd0
TheSnowfield <thesnowfield@moezsoft.com>
2022-03-03 03:05:50 +0800

    More extensions

---
+++ Utils/Extensions/DateTimeExt.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace Konata.Core.Utils.Extensions;
+
+public static class DateTimeExt
+{
+    /// <summary>
+    /// Convert DateTime to unix time
+    /// </summary>
+    /// <param name="time"></param>
+    /// <returns></returns>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static long Epoch(this DateTime time)
+        => (time.Ticks - 621355968000000000) / 10000;
+}
