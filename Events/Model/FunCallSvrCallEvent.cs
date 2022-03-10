48287e5c1bb2b14a0f4375c594fd7f13b0c3a4cd
TheSnowfield <thesnowfield@moezsoft.com>
2022-03-10 18:11:52 +0800

    For fun

---
+++ Events/Model/FunCallSvrCallEvent.cs
@@ -0,0 +1,35 @@
+﻿namespace Konata.Core.Events.Model;
+
+internal class FunCallSvrCallEvent : ProtocolEvent
+{
+    /// <summary>
+    /// Call uin
+    /// </summary>
+    public uint CallUin { get; }
+
+    private FunCallSvrCallEvent(uint callUin) : base(6000, true)
+    {
+        CallUin = callUin;
+    }
+    
+    private FunCallSvrCallEvent(int resultCode) 
+        : base(resultCode)
+    {
+    }
+
+    /// <summary>
+    /// Construct event
+    /// </summary>
+    /// <param name="callUin"></param>
+    /// <returns></returns>
+    public static FunCallSvrCallEvent Create(uint callUin)
+        => new(callUin);
+
+    /// <summary>
+    /// Construct event result
+    /// </summary>
+    /// <param name="resultCode"></param>
+    /// <returns></returns>
+    public static FunCallSvrCallEvent Result(int resultCode)
+        => new(resultCode);
+}
