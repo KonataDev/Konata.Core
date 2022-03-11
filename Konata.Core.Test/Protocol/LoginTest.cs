0caa00af234cf3ed36a6e83807a67bebab34a107
TheSnowfield <thesnowfield@moezsoft.com>
2022-03-11 12:48:46 +0800

    Refactor project tree

---
+++ Konata.Core.Test/Protocol/LoginTest.cs
@@ -0,0 +1,17 @@
+using NUnit.Framework;
+
+namespace Konata.Core.Test.Protocol;
+
+public class LoginTest
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
