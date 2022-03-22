f8f1fd318dd5b0404347243da6cb0ba5c192807c
TheSnowfield <thesnowfield@moezsoft.com>
2022-03-22 17:30:19 +0800

    Sharp svr

---
+++ Konata.Core/Events/Model/SharpSvrEvent.cs
@@ -0,0 +1,76 @@
+﻿// ReSharper disable MemberCanBePrivate.Global
+// ReSharper disable UnusedAutoPropertyAccessor.Global
+
+namespace Konata.Core.Events.Model;
+
+internal class SharpSvrEvent : ProtocolEvent
+{
+    /// <summary>
+    /// Self uin
+    /// </summary>
+    public uint SelfUin { get; }
+
+    /// <summary>
+    /// Call uin
+    /// </summary>
+    public uint CallUin { get; }
+
+    /// <summary>
+    /// Call status
+    /// </summary>
+    public CallStatus Status { get; }
+
+    private SharpSvrEvent(uint selfUin, uint callUin)
+        : base(6000, true)
+    {
+        SelfUin = selfUin;
+        CallUin = callUin;
+        Status = CallStatus.CallOut;
+    }
+
+    private SharpSvrEvent(int resultCode)
+        : base(resultCode)
+    {
+    }
+    
+    private SharpSvrEvent(int resultCode)
+        : base(resultCode)
+    {
+    }
+
+    /// <summary>
+    /// Construct event
+    /// </summary>
+    /// <param name="selfUin"></param>
+    /// <param name="callUin"></param>
+    /// <returns></returns>
+    public static SharpSvrEvent CallOut(uint selfUin, uint callUin)
+        => new(selfUin, callUin);
+
+    /// <summary>
+    /// Construct event
+    /// </summary>
+    /// <param name="e"></param>
+    /// <returns></returns>
+    public static SharpSvrEvent Ack(SharpSvrEvent e)
+        => new();
+    
+    /// <summary>
+    /// Construct event result
+    /// </summary>
+    /// <param name="resultCode"></param>
+    /// <returns></returns>
+    public static SharpSvrEvent Result(int resultCode)
+        => new(resultCode);
+
+    public static SharpSvrEvent PushAck(long roomId, uint friendUin)
+        => 
+    
+    
+    public enum CallStatus
+    {
+        Ack,
+        CallIn,
+        CallOut,
+    }
+}
